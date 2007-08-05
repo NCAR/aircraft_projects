a4b2cfd26afe294ab20e3bbb9e36a83779b819e1
Gordon Maclean <maclean@ucar.edu>
2007-08-05 19:59:17 +0000

    cleanup, do dns lookup to check for success
    svn path=/; revision=3738

--- Systems/scripts/run_ddclient.sh
+++ Systems/scripts/run_ddclient.sh
@@ -2,9 +2,26 @@
 
 # Run ddclient until success.
 
+# To test this script, uncomment "set -x" and run it by hand like so:
+# ./run_ddclient.sh ppp0 38400 12.42.105.41 12.42.104.9 ppp0
+#
 # set -x
 
+if [ $# -lt 4 ]; then
+    echo "$0 if baud local_IP remote_IP if"
+    echo "example: $0 ppp0 38400 12.42.105.41 12.42.104.9 ppp0"
+    logger -t ddclient "$0 : no arguments"
+    exit 1
+fi
+
 PATH=/usr/sbin:/sbin:$PATH
+
+logger -t ddclient $0 $*
+
+if ! which ddclient > /dev/null; then
+        logger -t ddclient "Cannot find ddclient. Install it in /usr/sbin or /sbin"
+        exit 1
+fi
 
 # clean up any old run_ddclient.sh scripts
 script=`basename $0`
@@ -23,47 +40,70 @@ IP=${IP:-$PPP_LOCAL}
 IP=${IP:-$IPLOCAL}
 IP=${IP:-$4}
 
-logger -t $0 $*
 case "$IP" in
 10.* | 172.1[6-9].* | 172.2[0-9].* | 172.3[0-1].* | 192.168.*)
         logger -t ddclient "$* : IP=$IP is a private address, cannot update"
-        logger -t ppp "$* : IP=$IP is a private address, cannot update"
         exit 1
         ;;
 "")
         logger -t ddclient "$* : No local IP given so cannot update"
-	logger -t ppp "$* : No local IP given so cannot update"
         exit 1
 	;;
-*)      ;;
+*)      
+        ;;
 esac
 
 [ -d /var/cache/ddclient ] || mkdir /var/cache/ddclient
 
-if ! which ddclient > /dev/null; then
-        logger -t ddclient "Cannot find ddclient. Install it in /usr/sbin or /sbin"
-        logger -t ppp "Cannot find ddclient. Install it in /usr/sbin or /sbin"
-        exit 1
-fi
+good_dns=false
+for (( ncheck=0; ncheck < 5; ncheck++ )); do
 
-for (( ntry=10; ntry > 0; ntry-- )); do
-        # every other time try members.dyndns.org, otherwise a fixed IP.
-        server=members.dyndns.org
-        if [ $(($ntry % 2)) -eq 0 ]; then
-            host members.dyndns.org > /dev/null || server=63.208.196.96 
-        else
-            server=63.208.196.96 
+    # use -R N option of host command to retry up to N requests
+    case `hostname` in
+    hercules*)
+        dname=rafc130.dyndns.org
+        ;;
+    hyper*)
+        dname=rafgv.dyndns.org
+        ;;
+    esac
+
+    if [ -n "$dname" ]; then
+        if ip=(`host -R 3 $dname`); then
+            # 4th word of host output is IP address
+            [ ${#ip[*]} -gt 3 -a "${ip[3]}" == "$IP" ] && good_dns=true
         fi
+    else
+        logger -t ddclient "Don't recognize hostname `hostname`, can't check ddclient success"
+    fi
 
-        cmd="ddclient -daemon=0 -syslog -use=if -if=$1 -server $server"
-        logger -t ddclient "$cmd"
-        logger -t ppp "$cmd"
-        eval $cmd && break
+    if $good_dns; then
+        logger -t ddclient "Lookup of $dname returns $IP. Running ddclient not necessary"
+        break
+    fi
 
-        # ddclient complains if you try to update more than every 5 minutes.
-        # However, I think it keeps track of that in the local cache,
-        # rather than on the server, so if we were devious we could
-        # delete the cache.
-        sleep 305
+    # wait again if not first time through
+    [ $ncheck -gt 0 ] && sleep 305
+
+    for (( ntry=0; ntry < 10; ntry++ )); do
+
+            # every other time try members.dyndns.org, otherwise a fixed IP.
+            server=members.dyndns.org
+            if [ $(($ntry % 2)) -eq 0 ]; then
+                host -R 2 members.dyndns.org > /dev/null || server=63.208.196.96 
+            else
+                server=63.208.196.96 
+            fi
+
+            cmd="ddclient -daemon=0 -syslog -use=if -if=$1 -server $server"
+            logger -t ddclient "$cmd"
+            eval $cmd && break
+
+            # ddclient complains if you try to update more than every
+            # 5 minutes. However, I think it keeps track of that in the
+            # local cache, rather than on the server, so if we were
+            # devious we could delete the cache.
+            sleep 305
+    done
 done
 
