b0d4a580967c49ce840978ef516d1958568bc55f
Dan Lagreca <dlagreca@ucar.edu>
2009-12-18 17:30:33 +0000

    Changed xmlrpc2shell to fork shell commands.
    svn path=/; revision=4739

--- Systems/scripts/xmlrpc2shell.py
+++ Systems/scripts/xmlrpc2shell.py
@@ -4,15 +4,18 @@ import SimpleXMLRPCServer, os
 PORT=30009
 
 def do_cmd(params):
-        call= "gnome-terminal --window-with-profile=\"%s\" -t \"%s\" -x %s" % (params)
-        #call= "xterm -hold -T \"%s\" -e \"%s\"" % (params)
-        os.system(call)
-        return 0
+    call= "gnome-terminal --window-with-profile=\"%s\" -t \"%s\" -x %s " % (params)
+    os.system(call)
 
 #The server object
 class shell_com_server:
     def shell_exec(self, p,t,c):
-        return do_cmd((p,t,c))
+
+	if (os.forkpty()[0] == 0):
+            do_cmd((p,t,c))
+
+	else:
+            return "Launched %s"%(c)
 
 scs = shell_com_server()
 serv = SimpleXMLRPCServer.SimpleXMLRPCServer(("localhost", PORT))
