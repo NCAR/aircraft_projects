4698002863f49e88ba613ac311f6b3e04d599d8c
Gordon Maclean <maclean@ucar.edu>
2010-01-28 18:43:00 +0000

    svn path=/; revision=4786

--- Systems/servers/raf-ads-user/home/ads/bin/svn-ask-username.sh
+++ Systems/servers/raf-ads-user/home/ads/bin/svn-ask-username.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # echo "nargs=$#"
-if [ "$1" == ci -o "$1" == commit ] && ! echo "$@" | fgrep -q -- --username; then
+if [[ `id -un` == ads && ("$1" == ci || "$1" == commit) ]] && ! echo "$@" | fgrep -q -- --username; then
 	read -p "enter --username for svn checkin: " uname
-	svn --username $uname "$@"
+	\svn --username $uname "$@"
 else
-	svn "$@"
+	\svn "$@"
 fi
