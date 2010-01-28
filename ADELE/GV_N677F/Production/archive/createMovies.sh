1d91ccdcedbee50f4f14d19041b5f38fc973ec80
Janine Aquino <janine@ucar.edu>
2010-01-28 20:48:23 +0000

    Archive movie generation code for ADELE
    svn path=/; revision=1448

---
+++ ADELE/GV_N677F/Production/archive/createMovies.sh
@@ -0,0 +1,10 @@
+#!/bin/csh
+./combineCameras.pl adeleParamFile rf01
+./combineCameras.pl adeleParamFile rf04
+./combineCameras.pl adeleParamFile rf06
+./combineCameras.pl adeleParamFile rf07
+./combineCameras.pl adeleParamFile rf08
+./combineCameras.pl adeleParamFile rf10
+./combineCameras.pl adeleParamFile rf11
+./combineCameras.pl adeleParamFile rf12
+./combineCameras.pl adeleParamFile tf03
