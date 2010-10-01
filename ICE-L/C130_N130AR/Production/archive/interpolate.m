eea562d2649ea34f7ab913ccf1d04383201f5842
Sean Stroble <stroble@ucar.edu>
2010-10-01 23:13:20 +0000

    Updated interpolate.m to ignore missing data (value of -99999). If insufficient data is available to interpolate a point it is set to the missing value.
    svn path=/; revision=1651

--- ICE-L/C130_N130AR/Production/archive/interpolate.m
+++ ICE-L/C130_N130AR/Production/archive/interpolate.m
@@ -34,14 +34,40 @@ for n=1:numel(Files)
     filein = fopen(filenameIn); %Open Input File
     header = fgetl(filein); %Get Header (First line)
     
+    fileout = fopen(filenameOut, 'w'); %Open Output File
+    fprintf(fileout, '%s\n',header); %Write Header
+    fclose(fileout); %Close File
     
+    fileout = fopen(filenameOut, 'a');
     line = fgetl(filein); %get first data line
     while (ischar(line))
        line = strrep(line, ',', ' ');
        split = sscanf(line, '%lg'); %look for floating point numbers
+       
        if (numel(split) == 2) %Make sure there are only 2 values
-           Tin(end+1) = split(1); %Save Time
-           O3in(end+1) = split(2); %Save O3
+           if (split(2) == -99999)
+               
+               if (numel(Tin) > 1)
+                   Tout = round(Tin(1)):1:round(Tin(end)); %Create clean list of times
+                   O3out = interp1(Tin, O3in, Tout, 'linear', 'extrap'); %Interpolate O3in to Tout  
+
+                   dlmwrite(filenameOut, [Tout',O3out'], '-append'); %Save Data
+
+               elseif (numel(Tin) == 1)
+                   fprintf(fileout, '%g,%g\n',round(Tin(1)),-99999); %Write Line
+                   
+               end
+               
+               %fileout = fopen(filenameOut, 'a'); %Open Output File
+               fprintf(fileout, '%g,%g\n',round(split(1)),-99999); %Write Line
+               %fclose(fileout); %Close File
+               
+               Tin = [];
+               O3in = [];
+           else
+               Tin(end+1) = split(1); %Save Time
+               O3in(end+1) = split(2); %Save O3
+           end
        else
            disp(strcat('UNKOWN LINE FORMAT: ', line));
        end
@@ -50,15 +76,21 @@ for n=1:numel(Files)
     
     fclose(filein);
     
-    Tout = round(Tin(1)):1:round(Tin(end)); %Create clean list of times
+    if (numel(Tin) > 1)
+        Tout = round(Tin(1)):1:round(Tin(end)); %Create clean list of times
+        O3out = interp1(Tin, O3in, Tout, 'linear', 'extrap'); %Interpolate O3in to Tout  
+ 
+        dlmwrite(filenameOut, [Tout',O3out'], '-append'); %Save Data
+ 
+        Tin = [];
+        O3in = [];
+    elseif (numel(Tin) == 1)
+        %fileout = fopen(filenameOut, 'a'); %Open Output File
+        fprintf(fileout, '%g,%g\n',round(Tin(1)),-99999); %Write Line
+        %fclose(fileout); %Close File
+    end
     
-    O3out = interp1(Tin, O3in, Tout, 'linear', 'extrap'); %Interpolate O3in to Tout
-    
-    fileout = fopen(filenameOut, 'w'); %Open Output File
-    fprintf(fileout, "%s\n",header); %Write Header
     fclose(fileout); %Close File
-
-    dlmwrite(filenameOut, [Tout',O3out'], '-append'); %Save Data
     
 end;
 
