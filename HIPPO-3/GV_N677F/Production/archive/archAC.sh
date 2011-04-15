671dbaa23a331249719e58e19bd45302990b30a1
Janine Aquino <janine@ucar.edu>
2011-04-15 18:19:06 +0000

    cleanup Production/archive
    svn path=/; revision=1940

---
+++ HIPPO-3/GV_N677F/Production/archive/archAC.sh
@@ -0,0 +1,47 @@
+#! /bin/csh -f
+##
+## This is an example script.
+## Set PROJECT and uncomment each command to archive that data type.
+#
+################
+##   Project   #
+################
+set PROJECT = "HIPPO-3"
+#echo "Make sure netCDF files have been reordered before archiving!!!"
+#
+#### ADS
+##/net/work/bin/scripts/mass_store/archAC/archAC.py ADS /scr/raf/Raw_Data/$PROJECT ads RAF
+#
+### LRT - done 6/14/2010
+#/net/work/bin/scripts/mass_store/archAC/archAC.py UNALTERED/LRT /scr/raf/Prod_Data/$PROJECT nc RAF
+#/net/work/bin/scripts/mass_store/archAC/archAC.py LRT /scr/raf/Prod_Data/$PROJECT nc ATDdata
+#
+#### CHAT
+##/net/work/bin/scripts/mass_store/archAC/archAC.py CHAT -t /scr/raf/Raw_Data/$PROJECT/Chat log RAF
+#
+#### PMS2D
+/net/work/bin/scripts/mass_store/archAC/archAC.py PMS2D /scr/raf/Raw_Data/$PROJECT/PMS2D 2d ATDdata
+##/net/work/bin/scripts/mass_store/archAC/archAC.py PMS2D /scr/raf/Raw_Data/$PROJECT/PMS2D 2d.gz ATDdata
+#
+#### CAMERA - This line creates hour tarballs of static camera images and
+#### puts them on the mss. 
+#/net/work/bin/scripts/mass_store/archAC/archAC.py CAMERA /scr/raf/Raw_Data/$PROJECT/Camera/flight_number_rf07/forward jpg ATDdata
+#/net/work/bin/scripts/mass_store/archAC/archAC.py CAMERA /scr/raf/Raw_Data/$PROJECT/Camera/flight_number_rf08/forward jpg ATDdata
+#/net/work/bin/scripts/mass_store/archAC/archAC.py CAMERA /scr/raf/Raw_Data/$PROJECT/Camera/flight_number_rf09/forward jpg ATDdata
+#/net/work/bin/scripts/mass_store/archAC/archAC.py CAMERA /scr/raf/Raw_Data/$PROJECT/Camera/flight_number_rf10/forward jpg ATDdata
+#/net/work/bin/scripts/mass_store/archAC/archAC.py CAMERA /scr/raf/Raw_Data/$PROJECT/Camera/flight_number_rf11/forward jpg ATDdata
+#/net/work/bin/scripts/mass_store/archAC/archAC.py CAMERA /scr/raf/Raw_Data/$PROJECT/Camera/flight_number_tf01/forward jpg ATDdata
+#/net/work/bin/scripts/mass_store/archAC/archAC.py CAMERA /scr/raf/Raw_Data/$PROJECT/Camera/flight_number_tf02/forward jpg ATDdata
+#/net/work/bin/scripts/mass_store/archAC/archAC.py CAMERA /scr/raf/Raw_Data/$PROJECT/Camera jpg ATDdata
+#
+#### MOVIES
+#### Don't archive preliminary movies to MSS, but do put them in codiac and
+#### make them orderable.
+#
+#### DGPS
+##/net/work/bin/scripts/mass_store/archAC/archAC.py DGPS /scr/raf/Raw_Data/$PROJECT/dgps ads ATDdata
+#
+#### SID2H
+##/net/work/bin/scripts/mass_store/archAC/archAC.py SID2H -t /scr/raf/Raw_Data/$PROJECT/sid2h srd RAF
+#
+#### VCSEL
---
+++ HIPPO-3/GV_N677F/Production/archive/createMovies.sh
@@ -0,0 +1,12 @@
+#!/bin/csh
+#/net/work/bin/converters/createMovies/combineCameras.pl 516.ParamFile rf01
+#/net/work/bin/converters/createMovies/combineCameras.pl 516.ParamFile rf02
+#/net/work/bin/converters/createMovies/combineCameras.pl 516.ParamFile rf03
+#/net/work/bin/converters/createMovies/combineCameras.pl 516.ParamFile rf04
+#/net/work/bin/converters/createMovies/combineCameras.pl 516.ParamFile rf05
+#/net/work/bin/converters/createMovies/combineCameras.pl 516.ParamFile rf06
+/net/work/bin/converters/createMovies/combineCameras.pl 516.ParamFile rf07
+/net/work/bin/converters/createMovies/combineCameras.pl 516.ParamFile rf08
+/net/work/bin/converters/createMovies/combineCameras.pl 516.ParamFile rf09
+/net/work/bin/converters/createMovies/combineCameras.pl 516.ParamFile rf10
+/net/work/bin/converters/createMovies/combineCameras.pl 516.ParamFile rf11
