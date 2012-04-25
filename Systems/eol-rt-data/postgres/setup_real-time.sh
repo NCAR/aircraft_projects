21c55ff8b66f5ead08ffe50570147f9ed7e42ef1
John Wasinger <wasinger@ucar.edu>
2012-04-25 18:35:18 +0000

    Added creation of raf_lrt table. Added StartTime and EndTime to global attributes.
    svn path=/; revision=5765

--- Systems/eol-rt-data/postgres/setup_real-time.sh
+++ Systems/eol-rt-data/postgres/setup_real-time.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Dev note 'IF EXISTS' clause is only supported in Postgres version 9 and higher.
+
 unset HOST
 unset AIRCRAFT
 
@@ -30,29 +32,27 @@ cat << EOD | psql -h $HOST -U ads -d real-time-$AIRCRAFT
 SET client_encoding = 'UTF8';
 SET check_function_bodies = false;
 SET client_min_messages = warning;
-
 SET search_path = public, pg_catalog;
-
 SET default_tablespace = '';
-
 SET default_with_oids = false;
+SET statement_timeout = 0;
+SET escape_string_warning = off;
 
 --
 -- Name: global_attributes; Type: TABLE; Schema: public; Owner: ads; Tablespace: 
 --
 
+-- ALTER TABLE ONLY global_attributes DROP CONSTRAINT IF EXISTS global_attributes_pkey;
+-- DROP TABLE IF EXISTS global_attributes;
+
 DROP TABLE global_attributes;
+
 CREATE TABLE global_attributes (
     "key" text NOT NULL,
     value text
 );
 
-
-ALTER TABLE public.global_attributes OWNER TO ads;
-
---
--- Data for Name: global_attributes; Type: TABLE DATA; Schema: public; Owner: ads
---
+ALTER TABLE global_attributes OWNER TO ads;
 
 INSERT INTO global_attributes VALUES ('Source', 'NASA/NSERC Dryden');
 INSERT INTO global_attributes VALUES ('Platform', 'N817NA');
@@ -61,19 +61,11 @@ INSERT INTO global_attributes VALUES ('longitude_coordinate', 'GGLON');
 INSERT INTO global_attributes VALUES ('zaxis_coordinate', 'GGALT');
 INSERT INTO global_attributes VALUES ('time_coordinate', 'datetime');
 INSERT INTO global_attributes VALUES ('landmarks', '39.9088 -105.117 jeffco');
-
-
---
--- Name: global_attributes_pkey; Type: CONSTRAINT; Schema: public; Owner: ads; Tablespace: 
---
+INSERT INTO global_attributes VALUES ('EndTime', '');
+INSERT INTO global_attributes VALUES ('StartTime', '');
 
 ALTER TABLE ONLY global_attributes
     ADD CONSTRAINT global_attributes_pkey PRIMARY KEY ("key");
-
-
---
--- Name: update; Type: RULE; Schema: public; Owner: ads
---
 
 CREATE RULE "update" AS ON UPDATE TO global_attributes DO NOTIFY current;
 
@@ -81,7 +73,11 @@ CREATE RULE "update" AS ON UPDATE TO global_attributes DO NOTIFY current;
 -- Name: variable_list; Type: TABLE; Schema: public; Owner: ads; Tablespace: 
 --
 
+-- ALTER TABLE ONLY variable_list DROP CONSTRAINT IF EXISTS variable_list_pkey;
+-- DROP TABLE IF EXISTS variable_list;
+
 DROP TABLE variable_list;
+
 CREATE TABLE variable_list (
     name text NOT NULL,
     units text,
@@ -96,12 +92,7 @@ CREATE TABLE variable_list (
     data_quality text
 );
 
-
-ALTER TABLE public.variable_list OWNER TO ads;
-
---
--- Data for Name: variable_list; Type: TABLE DATA; Schema: public; Owner: ads
---
+ALTER TABLE variable_list OWNER TO ads;
 
 INSERT INTO variable_list VALUES ('GGLAT',       'degree_N', '', 'Reference GPS Latitude',                             '', 1, '{1}', 0, '{}', -32767, 'Preliminary');
 INSERT INTO variable_list VALUES ('GGLON',       'degree_E', '', 'Reference GPS Longitude',                            '', 1, '{1}', 0, '{}', -32767, 'Preliminary');
@@ -134,12 +125,59 @@ INSERT INTO variable_list VALUES ('SOLZE',       'degree',   '', 'Solar Zenith A
 INSERT INTO variable_list VALUES ('Solar_El_AC', 'degree',   '', 'Sun Elevation from Aircraft',                        '', 1, '{1}', 0, '{}', -32767, 'Preliminary');
 INSERT INTO variable_list VALUES ('SOLAZ',       'degree_T', '', 'Sun Azimuth from Ground',                            '', 1, '{1}', 0, '{}', -32767, 'Preliminary');
 INSERT INTO variable_list VALUES ('Solar_Az_AC', 'degree_T', '', 'Sun Azimuth from Aircraft',                          '', 1, '{1}', 0, '{}', -32767, 'Preliminary');
---
--- Name: variable_list_pkey; Type: CONSTRAINT; Schema: public; Owner: ads; Tablespace: 
---
 
 ALTER TABLE ONLY variable_list
     ADD CONSTRAINT variable_list_pkey PRIMARY KEY (name);
+
+--
+-- Name: raf_lrt; Type: TABLE; Schema: public; Owner: ads; Tablespace: 
+--
+
+-- ALTER TABLE ONLY raf_lrt DROP CONSTRAINT IF EXISTS raf_lrt_pkey;
+-- DROP TABLE IF EXISTS raf_lrt;
+
+DROP TABLE raf_lrt;
+
+CREATE TABLE raf_lrt (
+    datetime timestamp without time zone NOT NULL,
+    gglat double precision,
+    gglon double precision,
+    ggalt double precision,
+    wgsalt double precision,
+    paltf double precision,
+    hgm232 double precision,
+    gsf double precision,
+    tasx double precision,
+    ias double precision,
+    mach_a double precision,
+    vspd double precision,
+    thdg double precision,
+    tkat double precision,
+    drfta double precision,
+    pitch double precision,
+    roll double precision,
+    sslip double precision,
+    attack double precision,
+    atx double precision,
+    dpxc double precision,
+    ttx double precision,
+    psxc double precision,
+    qcxc double precision,
+    pcab double precision,
+    wsc double precision,
+    wdc double precision,
+    wic double precision,
+    solze double precision,
+    solar_el_ac double precision,
+    solaz double precision,
+    solar_az_ac double precision
+);
+
+ALTER TABLE raf_lrt OWNER TO ads;
+
+ALTER TABLE ONLY raf_lrt
+    ADD CONSTRAINT raf_lrt_pkey PRIMARY KEY (datetime);
+
 --
 -- PostgreSQL database dump complete
 --
