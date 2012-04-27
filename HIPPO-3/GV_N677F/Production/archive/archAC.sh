fa46ab323d3ca29ad0761b86283a84349ad08a41
Chris Webster <cjw@ucar.edu>
2012-04-27 19:38:25 +0000

    Blankouts - blank derived DPXC, not raw DP. latest log files. Adjust Flight files for AQRATIO changes (LAMS pcors).
    svn path=/; revision=2492

--- HIPPO-3/GV_N677F/Production/archive/archAC.sh
+++ HIPPO-3/GV_N677F/Production/archive/archAC.sh
@@ -14,13 +14,13 @@ set PROJECT = "HIPPO-3"
 #
 ### LRT - done 6/14/2010
 #/net/work/bin/scripts/mass_store/archAC/archAC.py UNALTERED/LRT /scr/raf/Prod_Data/$PROJECT nc RAF
-#/net/work/bin/scripts/mass_store/archAC/archAC.py LRT /scr/raf/Prod_Data/$PROJECT nc ATDdata
+#/net/work/bin/scripts/mass_store/archAC/archAC.py LRT /scr/raf/Prod_Data/$PROJECT \.nc EOL stroble@ucar.edu
 #
 #### CHAT
 ##/net/work/bin/scripts/mass_store/archAC/archAC.py CHAT -t /scr/raf/Raw_Data/$PROJECT/Chat log RAF
 #
 #### PMS2D
-/net/work/bin/scripts/mass_store/archAC/archAC.py PMS2D /scr/raf/Raw_Data/$PROJECT/PMS2D 2d ATDdata
+#/net/work/bin/scripts/mass_store/archAC/archAC.py PMS2D /scr/raf/Raw_Data/$PROJECT/PMS2D 2d EOL
 ##/net/work/bin/scripts/mass_store/archAC/archAC.py PMS2D /scr/raf/Raw_Data/$PROJECT/PMS2D 2d.gz ATDdata
 #
 #### CAMERA - This line creates hour tarballs of static camera images and
@@ -45,3 +45,12 @@ set PROJECT = "HIPPO-3"
 ##/net/work/bin/scripts/mass_store/archAC/archAC.py SID2H -t /scr/raf/Raw_Data/$PROJECT/sid2h srd RAF
 #
 #### VCSEL
+
+###KML - done
+#/net/work/bin/scripts/mass_store/archAC/archAC.py KML /scr/raf/Prod_Data/$PROJECT/kml kml EOL stroble@ucar.edu
+
+### NASA_AMES
+#/net/work/bin/scripts/mass_store/archAC/archAC.py NASA_AMES /scr/raf/Prod_Data/$PROJECT/NASA_AMES gz EOL stroble@ucar.edu
+
+### MTP
+/net/work/bin/scripts/mass_store/archAC/archAC.py MTP /scr/raf/Prod_Data/$PROJECT/mtp NGV EOL janine@ucar.edu
--- HIPPO-3/GV_N677F/Production/archive/createMovies.sh
+++ HIPPO-3/GV_N677F/Production/archive/createMovies.sh
@@ -1,12 +1,28 @@
 #!/bin/csh
-#/net/work/bin/converters/createMovies/combineCameras.pl 516.ParamFile rf01
-#/net/work/bin/converters/createMovies/combineCameras.pl 516.ParamFile rf02
-#/net/work/bin/converters/createMovies/combineCameras.pl 516.ParamFile rf03
-#/net/work/bin/converters/createMovies/combineCameras.pl 516.ParamFile rf04
-#/net/work/bin/converters/createMovies/combineCameras.pl 516.ParamFile rf05
-#/net/work/bin/converters/createMovies/combineCameras.pl 516.ParamFile rf06
-/net/work/bin/converters/createMovies/combineCameras.pl 516.ParamFile rf07
-/net/work/bin/converters/createMovies/combineCameras.pl 516.ParamFile rf08
-/net/work/bin/converters/createMovies/combineCameras.pl 516.ParamFile rf09
-/net/work/bin/converters/createMovies/combineCameras.pl 516.ParamFile rf10
-/net/work/bin/converters/createMovies/combineCameras.pl 516.ParamFile rf11
+#/net/work/bin/converters/createMovies/combineCameras.pl HIPPO-3.ParamFile rf01
+#/net/work/bin/converters/createMovies/combineCameras.pl HIPPO-3.ParamFile rf02
+#/net/work/bin/converters/createMovies/combineCameras.pl HIPPO-3.ParamFile rf03
+#/net/work/bin/converters/createMovies/combineCameras.pl HIPPO-3.ParamFile rf04
+#/net/work/bin/converters/createMovies/combineCameras.pl HIPPO-3.ParamFile rf05
+#/net/work/bin/converters/createMovies/combineCameras.pl HIPPO-3.ParamFile rf06
+#/net/work/bin/converters/createMovies/combineCameras.pl HIPPO-3.ParamFile rf07
+#/net/work/bin/converters/createMovies/combineCameras.pl HIPPO-3.ParamFile rf08
+#/net/work/bin/converters/createMovies/combineCameras.pl HIPPO-3.ParamFile rf09
+#/net/work/bin/converters/createMovies/combineCameras.pl HIPPO-3.ParamFile rf10 
+#/net/work/bin/converters/createMovies/combineCameras.pl HIPPO-3.ParamFile rf11
+
+################################################################################
+# THIS IS A HIGH-RES VERSION OF A SUBSET OF RF06 CREATED FOR a gentleman at the 
+# American Academy of Science.
+#
+# I considered feeding the files in
+# /scr/raf/Raw_Data/HIPPO-3/movie_for_AAS/flight_number_rf06/forward directly 
+# into ffmpeg because the resolution is the same, but then realized that the 
+# camera software sharpens the image so decided to run that first (as usual).
+#
+# ffmpeg command:
+#/net/work/bin/converters/createMovies/ffmpeg -passlogfile ./ffmpeg_rf06 -r 15 -b 1500000 -y
+#-pass 2 -i /scr/raf/Prod_Data/HIPPO-3/Movies/for_AAS/AnnotatedImages_rf06/%05d.jpg
+#/scr/raf/Prod_Data/HIPPO-3/Movies/for_AAS/rf06.100405.232355_004159.mp4
+################################################################################
+/net/work/bin/converters/createMovies/combineCameras.pl HIPPO-3.for_AAS.ParamFile rf06
