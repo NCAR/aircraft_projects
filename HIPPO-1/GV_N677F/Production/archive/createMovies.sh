1014a98054f76062f11ea4215f7768d79a6800ed
Chris Webster <cjw@ucar.edu>
2012-04-27 19:34:44 +0000

    Blanksouts - blank out DPXC and not the raw signal. Latest log files.
    svn path=/; revision=2490

--- HIPPO-1/GV_N677F/Production/archive/createMovies.sh
+++ HIPPO-1/GV_N677F/Production/archive/createMovies.sh
@@ -1,10 +1,10 @@
 #!/bin/csh
-#/net/work/bin/converters/createMovies/combineCameras.pl HIPPO1.ParamFile RF02
-#/net/work/bin/converters/createMovies/combineCameras.pl HIPPO1.ParamFile RF03
-#/net/work/bin/converters/createMovies/combineCameras.pl HIPPO1.ParamFile RF04
-#/net/work/bin/converters/createMovies/combineCameras.pl HIPPO1.ParamFile RF05
-#/net/work/bin/converters/createMovies/combineCameras.pl HIPPO1.ParamFile RF06
-#/net/work/bin/converters/createMovies/combineCameras.pl HIPPO1.ParamFile RF07
-#/net/work/bin/converters/createMovies/combineCameras.pl HIPPO1.ParamFile RF08
-#/net/work/bin/converters/createMovies/combineCameras.pl HIPPO1.ParamFile RF10
-/net/work/bin/converters/createMovies/combineCameras.pl HIPPO1.ParamFile RF11
+nice +19 /net/work/bin/converters/createMovies/combineCameras.pl HIPPO1.ParamFile.rf02 RF02
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl HIPPO1.ParamFile.rf03 RF03
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl HIPPO1.ParamFile.rf04 RF04
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl HIPPO1.ParamFile.rf05 RF05
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl HIPPO1.ParamFile.rf06 RF06
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl HIPPO1.ParamFile.rf07 RF07
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl HIPPO1.ParamFile.rf08 RF08
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl HIPPO1.ParamFile.rf10 RF10
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl HIPPO1.ParamFile.rf11 RF11
