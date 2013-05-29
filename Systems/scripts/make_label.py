f6b74399e94e49da3f2b3f8a3a3f75f8421977ad
Chris Webster <cjw@ucar.edu>
2013-05-29 15:49:39 +0000

    Remove all the DC3 specific stuff.
    svn path=/; revision=6727

--- Systems/scripts/make_label.py
+++ Systems/scripts/make_label.py
@@ -37,10 +37,9 @@ except:
 
 # Initialization 
 #  *******************  Modify The Following *********************
-#local_image_dir  = '/var/www/html/flight_data/images/'
-local_image_dir  = '/home/local/Systems/scripts/images/'
-labelfile        = local_image_dir+'overlayLegend.gif'
-font             = '/usr/local/idv/jre/lib/fonts/LucidaTypewriterRegular.ttf'
+local_image_dir  = '/var/www/html/flight_data/images/'
+labelfile        = local_image_dir+'time_label.jpg'
+font             = '/usr/java/latest/lib/fonts/LucidaTypewriterRegular.ttf'
 
 # **************** End of Modify section ************************
 
@@ -50,38 +49,6 @@ conusFile = ''
 ltngFile = ''
 visFile = ''
 
-con = pg.connect(dbname=database, host=dbhost, user='ads')
-querres = con.query("select value from mission_control where key='cappi'")
-cappilst = querres.getresult()
-if len(cappilst) == 0:
-    cappiFile = "CAPPI: Off\\n"
-    conusFile = "NWS  : Off\\n"
-else:
-    cappi = (cappilst[0])[0]
-    if cappi == 'off':
-        cappiFile = "CAPPI: Off\\n"
-        conusFile = "NWS  : Off\\n"
-
-querres = con.query("select value from mission_control where key='lightning'")
-ltnglst = querres.getresult()
-if len(ltnglst) == 0:
-    ltngFile = "LMA  : Off\\n"
-else:
-    ltng = (ltnglst[0])[0]
-    if ltng == 'off':
-        ltngFile = "LMA  : Off\\n"
-
-querres = con.query("select value from mission_control where key='region'")
-regionlst = querres.getresult()
-if len(regionlst) == 0:
-    visFile = "VIS  : Off\\n"
-else:
-    vis = (regionlst[0])[0]
-    if vis == 'off':
-        visFile = "VIS  : Off\\n"
-
-con.close()
-
 # Get the listing and search it for latest images of each type
 os.chdir(local_image_dir)
 
@@ -89,7 +56,7 @@ dirlist = os.listdir(local_image_dir)
 label = ''
 
 # Infra-red
-IRregx = 'ops.goes-13.*thermal-IR.jpg'  
+IRregx = 'ops.GOES-15.*thermal-IR.jpg'  
 irlist = getList(dirlist,IRregx)
 if len(irlist) != 0:
     irFile = irlist[len(irlist)-1]
@@ -102,7 +69,7 @@ else:
 # Visible
 if len(visFile) == 0:
     print "Making vis label"
-    VISregx = 'ops.goes-13.*ch1_vis.jpg'
+    VISregx = 'ops.GOES-15.*ch1_vis.jpg'
     vislist = getList(dirlist,VISregx)
     if len(vislist) != 0:
         visFile = vislist[len(vislist)-1]
@@ -124,47 +91,14 @@ if len(conusFile) == 0:
         conusFile = conuslist[len(conuslist)-1]
         conusElts = []
         conusElts = conusFile.split('.')
-        conusFile = "NWS  : "+conusElts[2] + '\\n'
+        conusFile = "NWS  : "+conusElts[2] + ''
     else:
-        conusFile = "NWS  : no file available\\n"
+        conusFile = "NWS  : no file available"
 
 label = label + conusFile
 
-#CAPPI gotta get date and height
-if len(cappiFile) == 0:
-    CAPPIregx = 'radar.NEXRAD_3D*'
-    cappilist = getList(dirlist,CAPPIregx)
-    if len(cappilist) != 0:
-        cappiFile = cappilist[len(cappilist)-1]
-        cappiElts = []
-        cappiElts = cappiFile.split('.')
-        cappiFt = []
-        cappiFt = cappiElts[3].split("_")
-        cappiFile = "CAPPI: "+cappiElts[2]+" - "+cappiFt[0]+'\\n'
-    else:
-        cappiFile = "CAPPI: no file available\\n"
-
-label = label + cappiFile
-
-#LMA
-if len(ltngFile) == 0:
-    LMAregx = 'research.*kft*'
-    lmalist = getList(dirlist,LMAregx)
-    if len(lmalist) != 0:
-        ltngFile = lmalist[len(lmalist)-1]
-        lmaElts = []
-        lmaElts = ltngFile.split('.')
-        lmaFt = []
-        lmaFt = lmaElts[3].split("_")
-        ltngFile = "LMA  : "+lmaElts[2]+" - "+lmaFt[1]+'\\n'
-    else:
-        ltngFile = "LMA  : no file available\\n"
-
-label = label + ltngFile
-
-
 
 # Generate the label gif image
-command = "convert -background lightblue -fill blue -font " + font + " -pointsize 36 label:'"+label+"' " + labelfile
+command = "convert -background lightblue -fill blue -font " + font + " -pointsize 18 label:'"+label+"' " + labelfile
 os.system(command)
 
