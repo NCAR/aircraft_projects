c1fe6e154919638ed98cd1aa8c62367a69f2d0ff
Chris Webster <cjw@ucar.edu>
2013-05-29 15:47:49 +0000

    URI update.
    svn path=/; revision=6726

--- Systems/scripts/get_ir_image_cron.py
+++ Systems/scripts/get_ir_image_cron.py
@@ -47,15 +47,15 @@ plane,tail = aircraft.split("_",1)
 local_image_dir  = '/var/www/html/flight_data/images/'
 image_type       = 'IR'
 busy_file        = 'BUSY_'+image_type
-ftp_site         = 'catalog1.eol.ucar.edu'
+ftp_site         = 'catalog.eol.ucar.edu'
 ftp_login        = 'anonymous'
 ftp_passwd       = ''
 #ftp_dir          = '/pub/incoming/OSM/C130/'
 ftp_dir          = '/pub/incoming/OSM/'+plane+'/'
 label            = 'sat_ir_label.jpg'
 #Assumes filename form is prefixYYYYMMDD*postfix
-prefix           = 'ops.goes-13.'  
-postfix		 = '.4km_ch4_thermal-IR.jpg' 
+prefix           = 'ops.GOES-15.'  
+postfix		 = '4km_ch4_thermal-IR.jpg' 
 osm_file_name    = "latest_ir.jpg"
 num_imgs_to_get  = 10 # Script will backfill this many images for loops
 # End of Initialization section
