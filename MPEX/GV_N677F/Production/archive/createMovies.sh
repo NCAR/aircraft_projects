71d441a0d0fc9ead9b0c95091ff13d64efd47617
Chris Webster <cjw@ucar.edu>
2013-11-03 14:47:56 +0000

    Janine's production files.
    svn path=/; revision=3237

---
+++ MPEX/GV_N677F/Production/archive/createMovies.sh
@@ -0,0 +1,12 @@
+#!/bin/csh 
+nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl MPEX.paramfile rf06
+nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl MPEX.paramfile rf07
+nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl MPEX.paramfile rf08
+nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl MPEX.paramfile rf09
+nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl MPEX.paramfile rf10
+nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl MPEX.paramfile rf11
+nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl MPEX.paramfile rf12
+nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl MPEX.paramfile rf13
+nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl MPEX.paramfile rf14
+nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl MPEX.paramfile rf15
+
