4ce367554699abe58690b88df93502975b2eb1a1
Janine Aquino <janine@ucar.edu>
2013-11-05 03:39:26 +0000

    Scripts to crate movies and archive data.
    svn path=/; revision=3260

---
+++ TORERO/GV_N677F/Production/archive/archAC.sh
@@ -0,0 +1,28 @@
+#! /bin/csh -f
+#
+# This is an example script.
+# Set PROJECT and uncomment each command to archive that data type.
+
+###############
+#   Project   #
+###############
+set PROJECT = "TORERO"
+
+### ADS
+#/net/work/bin/scripts/mass_store/archAC/archAC.py ADS /scr/raf/Raw_Data/$PROJECT ads EOL
+
+### PMS2D
+#/net/work/bin/scripts/mass_store/archAC/archAC.py PMS2D /scr/raf/Raw_Data/$PROJECT/PMS2D 2d EOL
+
+### CAMERA
+#/net/work/bin/scripts/mass_store/archAC/archAC.py CAMERA /scr/raf/Raw_Data/$PROJECT/camera_images/flight_number_ff01 jpg EOL
+#/net/work/bin/scripts/mass_store/archAC/archAC.py CAMERA /scr/raf/Raw_Data/$PROJECT/camera_images/flight_number_tf02 jpg EOL
+### LRT
+#/net/work/bin/scripts/mass_store/archAC/archAC.py LRT /scr/raf/Prod_Data/$PROJECT nc EOL
+
+### KML
+#/net/work/bin/scripts/mass_store/archAC/archAC.py KML /scr/raf/Prod_Data/$PROJECT kml EOL
+
+### MTP
+/net/work/bin/scripts/mass_store/archAC/archAC.py mtp /scr/raf/Prod_Data/$PROJECT NGV EOL cbsnyder@ucar.edu
+
---
+++ TORERO/GV_N677F/Production/archive/createMovies.sh
@@ -0,0 +1,22 @@
+#!/bin/csh 
+#nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl TORERO.paramfile ff01
+#nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl TORERO.paramfile ff02
+#nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl TORERO.paramfile rf01 
+#nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl TORERO.paramfile rf02
+#         /net/work/dev/jaa/createMovies/combineCameras.pl TORERO.paramfile rf03
+#nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl TORERO.paramfile rf04
+#nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl TORERO.paramfile rf05
+#nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl TORERO.paramfile rf06
+#nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl TORERO.paramfile rf07
+#nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl TORERO.paramfile rf08
+#nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl TORERO.paramfile rf09
+#nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl TORERO.paramfile rf10
+#nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl TORERO.paramfile rf11
+#nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl TORERO.paramfile rf12
+#nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl TORERO.paramfile rf13
+#nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl TORERO.paramfile rf14
+#nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl TORERO.paramfile rf15
+#nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl TORERO.paramfile rf16
+nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl TORERO.paramfile rf17
+#nice +19 /net/work/dev/jaa/createMovies/combineCameras.pl TORERO.paramfile tf02
+
