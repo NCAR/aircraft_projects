7ad77537b0f7054d5b39000c12551403f8b11c5e
Janine Aquino <janine@ucar.edu>
2013-11-05 03:29:53 +0000

    Scripts to archive and manage data.
    svn path=/; revision=3257

--- PodCertGV/GV_N677F/Production/archive/archAC.sh
+++ PodCertGV/GV_N677F/Production/archive/archAC.sh
@@ -10,23 +10,10 @@ set PROJECT = "PodCertGV"
 #echo "Make sure netCDF files have been reordered before archiving!!!"
 
 ### ADS
-/net/work/bin/scripts/mass_store/archAC/archAC.py ADS /scr/raf2/Raw_Data/$PROJECT ads RAF
+#/net/work/bin/scripts/mass_store/archAC/archAC.py ADS /scr/raf/Raw_Data/$PROJECT ads RAF
 
-### CHAT
-#/net/work/bin/scripts/mass_store/archAC/archAC.py CHAT -t /scr/raf2/Raw_Data/$PROJECT/Chat log RAF
+### MATLAB
+#/net/work/bin/scripts/mass_store/archAC/archAC.py MATLAB /scr/raf/Raw_Data/$PROJECT/GAC_data mat EOL
 
-### PMS2D
-#/net/work/bin/scripts/mass_store/archAC/archAC.py PMS2D /scr/raf/Raw_Data/$PROJECT/PMS2D 2d ATDdata
-#/net/work/bin/scripts/mass_store/archAC/archAC.py PMS2D /scr/raf/Raw_Data/$PROJECT/PMS2D 2d.gz ATDdata
-
-### CAMERA - use archcam.510, not this script.
-
-### MOVIES
-
-### DGPS
-#/net/work/bin/scripts/mass_store/archAC/archAC.py DGPS /scr/raf2/Raw_Data/$PROJECT/dgps ads ATDdata
-
-### SID2H
-#/net/work/bin/scripts/mass_store/archAC/archAC.py SID2H -t /scr/raf2/Raw_Data/$PROJECT/sid2h srd RAF
-
-### VCSEL
+### NetCDF
+/net/work/bin/scripts/mass_store/archAC/archAC.py NetCDF /scr/raf/Prod_Data/$PROJECT nc EOL
