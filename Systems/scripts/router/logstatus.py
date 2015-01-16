ced42f4b0b21e803f9a6ce7bfee5598d4e64ae7c
Gary Granger <granger@ucar.edu>
2015-01-16 21:05:58 +0000

    add entry script to log current router status to syslog
    svn path=/; revision=7579

---
+++ Systems/scripts/router/logstatus.py
@@ -0,0 +1,33 @@
+
+import sys
+sys.path.append("/home/local/raf/python")
+
+import logging
+from logging.handlers import SysLogHandler
+ 
+log = logging.getLogger("router/logstatus")
+log.setLevel(logging.INFO)
+ 
+handler = SysLogHandler(address='/dev/log', facility=SysLogHandler.LOG_USER)
+ 
+formatter = logging.Formatter('%(name)s: %(message)s')
+handler.setFormatter(formatter)
+ 
+log.addHandler(handler)
+
+
+# Instantiate a Router instance with the connection parameters,
+# collect it's status, then ask it to generate a log message.
+
+user = 'admin'
+pwd = 'password'
+host = '192.168.99.1'
+
+import raf.router
+
+router = raf.router.Router("localhost", 8130)
+router.setStatusHelper(raf.router.netgear.NetgearStatusHelper)
+router.updateStatus()
+
+log.info(router.getStatusMessage())
+
