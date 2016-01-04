34897b5426fe7bf6f3eba5f6af3d7228825a83ac
Chris Webster <cjw@ucar.edu>
2016-01-04 21:56:22 +0000

    Add 1 off DEEPWAVE scripts.
    svn path=/; revision=4835

---
+++ DEEPWAVE/GV_N677F/scripts/send_biff_to_ingest.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+#
+# This script attempts to send the compressed dropsonde files to ingest via ldm.
+#
+# This is set up to run out of a cron job every minute
+# */1 * * * * /home/local/Systems/scripts/send_biff_to_ingest.cron.py
+#
+import os
+import sys
+import glob
+import stat
+import syslog
+import tarfile
+
+# Clean up name of script for logging
+script = sys.argv[0][sys.argv[0].rfind("/")+1:]
+
+os.chdir('/mnt/r1/biff/')
+
+# This cron script is not re-entrant, bail out if still running.
+if os.path.isfile('BUSY'):
+  syslog.syslog(script+': exiting, BUSY')
+  sys.exit(1)
+os.system('touch BUSY')
+
+os.system('mkdir -p inserted')
+
+# NaContour_03-Jul-2014_7-7-33.png
+files=glob.glob('NaContour*.png')
+for file in files:
+
+    # Copy the files to the web dir so they can be viewed in a browser 
+    # onboard the aircraft.
+    os.system('/bin/cp '+file+' /var/www/html/lidar/.');
+
+    os.system('bzip2 '+file);
+
+files=glob.glob('*.bz2')
+
+# bail out if no files are found
+if (not files):
+# syslog.syslog(script+': exiting, nothing to send')
+  os.remove('BUSY')
+  sys.exit(1)
+
+# sort files by creation time
+files.sort(lambda x, y: cmp(os.path.getmtime(x),os.path.getmtime(y)))
+
+for file in files:
+  try:
+    # ensure that file is complete before inserting
+#    tarball = tarfile.open(file, 'r:bz2')
+#    tarfiles = tarball.getnames()
+#    tarball.close()
+    syslog.syslog(script+': '+file+' is complete.')
+
+    # mv file to another folder then insert it
+    os.rename(file, 'inserted/'+file)
+    os.system("/home/ldm/bin/pqinsert inserted/"+file)
+
+  except:
+    syslog.syslog(script+': '+file+' is incomplete... must still be FTPing down.')
+
+# remove busy flag
+os.remove('BUSY')
