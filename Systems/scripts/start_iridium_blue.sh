6da34ed60d6054e3bbdb815d875102aabf85af93
Chris Webster <cjw@ucar.edu>
2016-04-14 16:35:47 +0000

    Missed .csh...
    svn path=/; revision=8357

--- Systems/scripts/start_iridium_blue.sh
+++ Systems/scripts/start_iridium_blue.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gnome-terminal --title "IRIDIUM LOG" -x /home/local/Systems/scripts/show_iridium_log.csh &
+gnome-terminal --title "IRIDIUM LOG" -x /home/local/Systems/scripts/show_iridium_log.sh &
 
 gnome-terminal --title "IRIDIUM Launch window" -x sudo /home/local/Systems/scripts/run_iridium_blue
