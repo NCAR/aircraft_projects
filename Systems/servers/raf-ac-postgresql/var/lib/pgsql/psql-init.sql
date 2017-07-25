28ba1cc8da3b806c09589f41a8befc734bc2675c
Janine Aquino <janine@ucar.edu>
2017-07-25 23:18:59 +0000

    Add creation of real-time database. Clean up comments.
    svn path=/; revision=8816

--- Systems/servers/raf-ac-postgresql/var/lib/pgsql/psql-init.sql
+++ Systems/servers/raf-ac-postgresql/var/lib/pgsql/psql-init.sql
@@ -1,11 +1,24 @@
 --
--- PostgreSQL database init sequence for eol-rt-data. This is executed 
--- during rpm installation. To run manually, type 
+-- PostgreSQL database init sequence. Use this for both eol-rt-data and 
+-- installation on aircraft so keep it generic. platform-specific users
+-- and databases are handled in platform-specific RPMs. See 
+-- raf-ac-eolrtdata RPM.
+--
+-- This script is executed during rpm installation. To run manually, type
 -- 'cat psql-init.sql | psql'
 --
+
+--
 -- Create required users
+--   - users without roles default to read only
 --
 CREATE USER ads with LOGIN PASSWORD 'ads';
-CREATE USER janine with LOGIN PASSWORD 'janine';
+
 CREATE USER nimbus with LOGIN PASSWORD 'nimbus';
 ALTER ROLE nimbus superuser;
+
+--
+-- Create required databases
+--
+CREATE DATABASE real-time;
+
