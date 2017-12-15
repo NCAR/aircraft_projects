a704472d6373a63556ee189a65638fd9ff63b4fb
Janine Aquino <janine@ucar.edu>
2017-12-15 22:40:07 +0000

    Differentiate paths for running on groundstation or locally
    svn path=/; revision=5269

--- SOCRATES/GV_N677F/scripts/createMovies.sh
+++ SOCRATES/GV_N677F/scripts/createMovies.sh
@@ -1,25 +1,30 @@
 #!/bin/csh 
 ########
-#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile ff01
-#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile ff02
-#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile ff03
-#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf01
-#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf02
-#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf03
-#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf04
-#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf05
-#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf06
-#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf07
-#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf08
-#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf09
-#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf10
-#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf11
-#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf13
-#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf14
-#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf15
-#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf16
-#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf17
-#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf18
-#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf19
-nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile tf01
-#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile tf02
+# On barolo
+#setenv MDIR /net/work/bin/converters
+# On groundstation
+setenv MDIR /home/local/raf/instruments/camera
+
+#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile ff01
+#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile ff02
+#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile ff03
+#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf01
+#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf02
+#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf03
+#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf04
+#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf05
+#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf06
+#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf07
+#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf08
+#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf09
+#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf10
+#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf11
+#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf13
+#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf14
+#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf15
+#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf16
+#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf17
+#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf18
+#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf19
+nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile tf01
+#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile tf02
