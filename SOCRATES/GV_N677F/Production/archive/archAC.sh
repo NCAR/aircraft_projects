1ffab4320165b819fb1a2802a8a1464794c7cd8a
Janine Aquino <janine@ucar.edu>
2017-12-15 22:23:37 +0000

    Initial setup for movie generation and data archival
    svn path=/; revision=5268

---
+++ SOCRATES/GV_N677F/Production/archive/archAC.sh
@@ -0,0 +1,91 @@
+#! /bin/csh -f
+#
+# This is an example script.
+# Set PROJECT and uncomment each command to archive that data type.
+
+###############
+#   Project   #
+###############
+set PROJECT = "SOCRATES"
+
+### ADS
+#/net/work/bin/scripts/mass_store/archAC/archAC.py ADS /scr/raf/Raw_Data/$PROJECT ads EOL/2018
+
+### CAMERA - RUN THIS LINE FROM /scr/raf/Raw_Data/ so temp files will go
+### there and not fill up /net/jlocal
+#/net/work/bin/scripts/mass_store/archAC/archAC.py CAMERA -r /scr/raf/Raw_Data/$PROJECT/camera_images jpg EOL/2018
+
+########################## Preliminary Data Files ##########################
+### Preliminary LRT 
+#/net/work/bin/scripts/mass_store/archAC/archAC.py field_phase/LRT /scr/raf/Raw_Data/$PROJECT/field_phase/LRT nc EOL/2018
+#/net/work/bin/scripts/mass_store/archAC/archAC.py preliminary/LRT /scr/raf/Raw_Data/$PROJECT/preliminary/LRT nc EOL/2018
+
+### Preliminary LRT ICT
+#/net/work/bin/scripts/mass_store/archAC/archAC.py preliminary/ICARTT /scr/raf/Raw_Data/$PROJECT/preliminary/icartt asc EOL/2018
+
+### Preliminary KML
+#/net/work/bin/scripts/mass_store/archAC/archAC.py field_phase/KML /scr/raf/Raw_Data/$PROJECT/field_phase/LRT/kml ff02.kml EOL/2018
+
+### Preliminary HRT 
+#/net/work/bin/scripts/mass_store/archAC/archAC.py field_phase/HRT /scr/raf/Raw_Data/$PROJECT/field_phase/HRT nc EOL/2018
+
+### Preliminary HRT KML
+#/net/work/bin/scripts/mass_store/archAC/archAC.py field_phase/KMLh /scr/raf/Raw_Data/$PROJECT/field_phase/HRT kml EOL/2018
+
+### Preliminary CO ICT
+#/net/work/bin/scripts/mass_store/archAC/archAC.py FIELD_INTERNAL_ONLY/CO /scr/raf/Raw_Data/$PROJECT/field_phase/CO ict EOL/2018
+
+### Preliminary CO2CH4 ICT
+#/net/work/bin/scripts/mass_store/archAC/archAC.py FIELD_INTERNAL_ONLY/CO2CH4 /scr/raf/Raw_Data/$PROJECT/field_phase/CO2CH4 ict EOL/2018
+
+### Preliminary NONO2O3 ICT
+#/net/work/bin/scripts/mass_store/archAC/archAC.py FIELD_INTERNAL_ONLY/NONOyO3 /scr/raf/Raw_Data/$PROJECT/field_phase/NO_NOy_O3 ict EOL/2018
+
+### Preliminary TOGA ICT
+#/net/work/bin/scripts/mass_store/archAC/archAC.py FIELD_INTERNAL_ONLY/TOGA /scr/raf/Raw_Data/$PROJECT/field_phase/TOGA ict EOL/2018
+
+### Preliminary TOF-AMS ICT
+#/net/work/bin/scripts/mass_store/archAC/archAC.py FIELD_INTERNAL_ONLY/TOF-AMS /scr/raf/Raw_Data/$PROJECT/field_phase/CU-HRAMS ict EOL/2018
+
+### Preliminary HARP Actinic Flux ICT
+#/net/work/bin/scripts/mass_store/archAC/archAC.py FIELD_INTERNAL_ONLY/HARP /scr/raf/Raw_Data/$PROJECT/field_phase/HARP_Jvalues ict EOL/2018
+
+### Preliminary ARNOLD ICT
+#/net/work/bin/scripts/mass_store/archAC/archAC.py FIELD_INTERNAL_ONLY/ARNOLD /scr/raf/Raw_Data/$PROJECT/field_phase/ARNOLD ict EOL/2018
+
+
+########################## Production Data Files ##########################
+### LRT
+#/net/work/bin/scripts/mass_store/archAC/archAC.py LRT /scr/raf/Prod_Data/$PROJECT Z.nc EOL/2018
+
+### KML
+#/net/work/bin/scripts/mass_store/archAC/archAC.py KML /scr/raf/Prod_Data/$PROJECT kml EOL/2018
+
+### LRT ICT
+/net/work/bin/scripts/mass_store/archAC/archAC.py ICARTT /scr/raf/Prod_Data/$PROJECT asc EOL/2018
+
+### PMS2D
+#/net/work/bin/scripts/mass_store/archAC/archAC.py PMS2D /scr/raf/Raw_Data/$PROJECT/PMS2D 2d EOL/2018
+
+
+### HRT
+#/net/work/bin/scripts/mass_store/archAC/archAC.py HRT /scr/raf/Prod_Data/$PROJECT/HRT nc EOL/2018
+
+### HRT KML
+#/net/work/bin/scripts/mass_store/archAC/archAC.py KMLh /scr/raf/Prod_Data/$PROJECT/HRT kml EOL/2018
+
+### CO ICT
+#/net/work/bin/scripts/mass_store/archAC/archAC.py CO /scr/raf/Prod_Data/$PROJECT/CO ict EOL/2018
+
+### CO2CH4 ICT
+#/net/work/bin/scripts/mass_store/archAC/archAC.py CO2CH4 /scr/raf/Prod_Data/$PROJECT/CO2CH4 ict EOL/2018
+
+### HARP ICT
+#/net/work/bin/scripts/mass_store/archAC/archAC.py HARP /scr/raf/Prod_Data/$PROJECT/HARP ict EOL/2018
+
+### GT-CIMS ICT
+#/net/work/bin/scripts/mass_store/archAC/archAC.py GT-CIMS /scr/raf/Prod_Data/$PROJECT/GT-CIMS ict EOL/2018
+
+### NONO2O3 ICT
+#/net/work/bin/scripts/mass_store/archAC/archAC.py NONO2O3 /scr/raf/Prod_Data/$PROJECT/NONO2O3 ict EOL/2018
+
---
+++ SOCRATES/GV_N677F/scripts/createMovies.sh
@@ -0,0 +1,25 @@
+#!/bin/csh 
+########
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile ff01
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile ff02
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile ff03
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf01
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf02
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf03
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf04
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf05
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf06
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf07
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf08
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf09
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf10
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf11
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf13
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf14
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf15
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf16
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf17
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf18
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf19
+nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile tf01
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile tf02
