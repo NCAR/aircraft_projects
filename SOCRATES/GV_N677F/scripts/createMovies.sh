e9c71e642c57e1a19f97024d72d95d049bdc2e8c
Janine Aquino <janine@ucar.edu>
2018-03-09 17:17:13 +0000

    Script for Jorgen to get GNI slide exposure times from GNI logs. Assumes logs are /scr/raf_Raw_Data/<PROJECT>/GNI/logs.
    svn path=/; revision=5336

--- SOCRATES/GV_N677F/scripts/createMovies.sh
+++ SOCRATES/GV_N677F/scripts/createMovies.sh
@@ -1,30 +1,29 @@
-#!/bin/csh 
-########
-# On barolo
-#setenv MDIR /net/work/bin/converters
-# On groundstation
-setenv MDIR /home/local/raf/instruments/camera
-
-#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile ff01
-#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile ff02
-#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile ff03
-#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf01
-#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf02
-#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf03
-#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf04
-#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf05
-#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf06
-#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf07
-#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf08
-#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf09
-#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf10
-#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf11
-#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf13
-#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf14
-#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf15
-#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf16
-#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf17
-#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf18
-#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile rf19
-nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile tf01
-#nice +19 ${MDIR}/createMovies/combineCameras.pl SOCRATES.paramfile tf02
+#!/bin/csh
+#######
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile ff01
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile ff02
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile ff03
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile ff04
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile ff05
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile ff06
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile ff07
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile ff08
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile ff09
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile ff10
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf01
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf02
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf03
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf04
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf05
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf06
+nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf07
+nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf08
+nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf09
+nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf10
+nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf11
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf12
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf13
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf14
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile rf15
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile tf01
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl SOCRATES.paramfile tf02
