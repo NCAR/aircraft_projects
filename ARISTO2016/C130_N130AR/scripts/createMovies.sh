ff57f06186dbd9e54aff8d164303cfb11dc6d6aa
Janine Aquino <janine@ucar.edu>
2018-04-16 22:43:49 +0000

    Movie generation scripts
    svn path=/; revision=5381

---
+++ ARISTO2016/C130_N130AR/scripts/createMovies.sh
@@ -0,0 +1,7 @@
+#/bin/csh 
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl ARISTO2016.paramfile rf01
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl ARISTO2016.paramfile rf02
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl ARISTO2016.paramfile rf03
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl ARISTO2016.paramfile rf04
+nice +19 /net/work/bin/converters/createMovies/combineCameras.pl ARISTO2016.paramfile rf05
+nice +19 /net/work/bin/converters/createMovies/combineCameras.pl ARISTO2016.paramfile rf06
---
+++ ARISTO2016/C130_N130AR/scripts/filterImages.sh
@@ -0,0 +1,15 @@
+#!/bin/csh
+
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/ARISTO2016/camera_images/flight_number_rf01/forward/" "-proj:ARISTO2016" "-s:15"
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/ARISTO2016/camera_images/flight_number_rf02/forward/" "-proj:ARISTO2016" "-s:15"
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/ARISTO2016/camera_images/flight_number_rf03/forward/" "-proj:ARISTO2016" "-s:15"
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/ARISTO2016/camera_images/flight_number_rf04/forward/" "-proj:ARISTO2016" "-s:15"
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/ARISTO2016/camera_images/flight_number_rf05/forward/" "-proj:ARISTO2016" "-s:15"
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/ARISTO2016/camera_images/flight_number_rf06/forward/" "-proj:ARISTO2016" "-s:15"
+
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/ARISTO2016/camera_images/flight_number_rf01/down/" "-proj:ARISTO2016" "-s:15"
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/ARISTO2016/camera_images/flight_number_rf02/down/" "-proj:ARISTO2016" "-s:15"
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/ARISTO2016/camera_images/flight_number_rf03/down/" "-proj:ARISTO2016" "-s:15"
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/ARISTO2016/camera_images/flight_number_rf04/down/" "-proj:ARISTO2016" "-s:15"
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/ARISTO2016/camera_images/flight_number_rf05/down/" "-proj:ARISTO2016" "-s:15"
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/ARISTO2016/camera_images/flight_number_rf06/down/" "-proj:ARISTO2016" "-s:15"
