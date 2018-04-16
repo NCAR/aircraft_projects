5f37b192c985b92f0eeb3941f2a61f9d66de75d6
Janine Aquino <janine@ucar.edu>
2018-04-16 22:57:07 +0000

    Movie generation scripts
    svn path=/; revision=5386

---
+++ ARISTO2017/GV_N677F/scripts/createMovies.sh
@@ -0,0 +1,8 @@
+#!/bin/csh 
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl ARISTO2017.paramfile rf01
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl ARISTO2017.paramfile rf02
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl ARISTO2017.paramfile rf03
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl ARISTO2017.paramfile rf04
+nice +19 /net/work/bin/converters/createMovies/combineCameras.pl ARISTO2017.paramfile rf05
+nice +19 /net/work/bin/converters/createMovies/combineCameras.pl ARISTO2017.paramfile rf06
+
---
+++ ARISTO2017/GV_N677F/scripts/filterImages.sh
@@ -0,0 +1,12 @@
+#/bin/csh
+
+# ARISTO2017 only flew a forward facing camera
+#/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/MITTS/camera_images/flight_number_rf01/forward/" "-proj:MITTS" "-s:15"
+#/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/MITTS/camera_images/flight_number_rf02/forward/" "-proj:MITTS" "-s:15"
+#/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/MITTS/camera_images/flight_number_rf03/forward/" "-proj:MITTS" "-s:15"
+#/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/MITTS/camera_images/flight_number_tf01/forward/" "-proj:MITTS" "-s:15"
+
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/ARISTO2017/camera_images/flight_number_rf01/forward/" "-proj:ARISTO2017" "-s:15"
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/ARISTO2017/camera_images/flight_number_rf02/forward/" "-proj:ARISTO2017" "-s:15"
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/ARISTO2017/camera_images/flight_number_rf03/forward/" "-proj:ARISTO2017" "-s:15"
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/ARISTO2017/camera_images/flight_number_rf04/forward/" "-proj:ARISTOR2017" "-s:15"
