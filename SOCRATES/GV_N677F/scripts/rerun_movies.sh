5a38a7e741b7ded6e933159bed001999abf71207
Janine Aquino <janine@ucar.edu>
2018-04-17 01:38:37 +0000

    Final movie scripts
    svn path=/; revision=5397

---
+++ SOCRATES/GV_N677F/scripts/rerun_movies.sh
@@ -0,0 +1,63 @@
+#!/bin/csh
+#script to run ffmpeg option with pixel format defined explicitly
+
+#rf01
+#/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf01/%05d.jpg -passlogfile ./ffmpeg_rf01 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 1 /scr/raf/Prod_Data/SOCRATES/Movies/rf01.180115.225417_060431.mp4
+#/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf01/%05d.jpg -passlogfile ./ffmpeg_rf01 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 2 /scr/raf/Prod_Data/SOCRATES/Movies/rf01.180115.225417_060431.mp4
+
+#rf02
+#/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf02/%05d.jpg -passlogfile ./ffmpeg_rf02 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 1 /scr/raf/Prod_Data/SOCRATES/Movies/rf02.180119.005134_074038.mp4 
+#/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf02/%05d.jpg -passlogfile ./ffmpeg_rf02 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 2 /scr/raf/Prod_Data/SOCRATES/Movies/rf02.180119.005134_074038.mp4 
+
+#rf03
+#/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf03/%05d.jpg -passlogfile ./ffmpeg_rf03 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 1 /scr/raf/Prod_Data/SOCRATES/Movies/rf03.180122.210944_041112.mp4
+#/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf03/%05d.jpg -passlogfile ./ffmpeg_rf03 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 2 /scr/raf/Prod_Data/SOCRATES/Movies/rf03.180122.210944_041112.mp4
+
+#rf04
+#/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf04/%05d.jpg -passlogfile ./ffmpeg_rf04 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 1 /scr/raf/Prod_Data/SOCRATES/Movies/rf04.180123.231926_054631.mp4
+#/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf04/%05d.jpg -passlogfile ./ffmpeg_rf04 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 2 /scr/raf/Prod_Data/SOCRATES/Movies/rf04.180123.231926_054631.mp4
+
+#rf05
+#/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf05/%05d.jpg -passlogfile ./ffmpeg_rf05 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 1 /scr/raf/Prod_Data/SOCRATES/Movies/rf05.180125.225508_055519.mp4
+#/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf05/%05d.jpg -passlogfile ./ffmpeg_rf05 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 2 /scr/raf/Prod_Data/SOCRATES/Movies/rf05.180125.225508_055519.mp4
+
+#rf06
+#/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf06/%05d.jpg -passlogfile ./ffmpeg_rf06 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 1 /scr/raf/Prod_Data/SOCRATES/Movies/rf06.180128.225430_054404.mp4
+#/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf06/%05d.jpg -passlogfile ./ffmpeg_rf06 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 2 /scr/raf/Prod_Data/SOCRATES/Movies/rf06.180128.225430_054404.mp4
+
+#rf07
+#/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf07/%05d.jpg -passlogfile ./ffmpeg_rf07 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 1 /scr/raf/Prod_Data/SOCRATES/Movies/rf07.180131.005831_082945.mp4
+#/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf07/%05d.jpg -passlogfile ./ffmpeg_rf07 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 2 /scr/raf/Prod_Data/SOCRATES/Movies/rf07.180131.005831_082945.mp4
+
+#rf08 
+#/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf08/%05d.jpg -passlogfile ./ffmpeg_rf08 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 1 /scr/raf/Prod_Data/SOCRATES/Movies/rf08.180203.231712_050613.mp4
+#/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf08/%05d.jpg -passlogfile ./ffmpeg_rf08 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 2 /scr/raf/Prod_Data/SOCRATES/Movies/rf08.180203.231712_050613.mp4
+
+#rf09
+#/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf09/%05d.jpg -passlogfile ./ffmpeg_rf09 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 1 /scr/raf/Prod_Data/SOCRATES/Movies/rf09.180204.225551_070154.mp4
+#/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf09/%05d.jpg -passlogfile ./ffmpeg_rf09 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 2 /scr/raf/Prod_Data/SOCRATES/Movies/rf09.180204.225551_070154.mp4
+
+#rf10 
+#/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf10/%05d.jpg -passlogfile ./ffmpeg_rf10 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 1 /scr/raf/Prod_Data/SOCRATES/Movies/rf10.180207.205445_051059.mp4
+#/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf10/%05d.jpg -passlogfile ./ffmpeg_rf10 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 2 /scr/raf/Prod_Data/SOCRATES/Movies/rf10.180207.205445_051059.mp4
+
+#rf11
+/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf11/%05d.jpg -passlogfile ./ffmpeg_rf11 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 1 /scr/raf/Prod_Data/SOCRATES/Movies/rf11.180217.014618_061718.mp4
+/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf11/%05d.jpg -passlogfile ./ffmpeg_rf11 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 2 /scr/raf/Prod_Data/SOCRATES/Movies/rf11.180217.014618_061718.mp4
+
+#rf12 
+/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf12/%05d.jpg -passlogfile ./ffmpeg_rf12 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 1 /scr/raf/Prod_Data/SOCRATES/Movies/rf12.180218.000811_075243.mp4
+/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf12/%05d.jpg -passlogfile ./ffmpeg_rf12 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 2 /scr/raf/Prod_Data/SOCRATES/Movies/rf12.180218.000811_075243.mp4
+
+#rf13
+/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf13/%05d.jpg -passlogfile ./ffmpeg_rf13 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 1 /scr/raf/Prod_Data/SOCRATES/Movies/rf13.180219.225931_063605.mp4
+/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf13/%05d.jpg -passlogfile ./ffmpeg_rf13 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 2 /scr/raf/Prod_Data/SOCRATES/Movies/rf13.180219.225931_063605.mp4
+
+#rf14
+/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf14/%05d.jpg -passlogfile ./ffmpeg_rf14 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 1 /scr/raf/Prod_Data/SOCRATES/Movies/rf14.180221.224816_064552.mp4
+/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf14/%05d.jpg -passlogfile ./ffmpeg_rf14 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 2 /scr/raf/Prod_Data/SOCRATES/Movies/rf14.180221.224816_064552.mp4
+
+#rf15
+/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf15/%05d.jpg -passlogfile ./ffmpeg_rf15 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 1 /scr/raf/Prod_Data/SOCRATES/Movies/rf15.180224.021609_075326.mp4
+/usr/bin/ffmpeg -i /scr/raf/Prod_Data/SOCRATES/Movies/AnnotatedImages_rf15/%05d.jpg -passlogfile ./ffmpeg_rf15 -r 15 -b:v 1500000 -pix_fmt yuv420p -y -pass 2 /scr/raf/Prod_Data/SOCRATES/Movies/rf15.180224.021609_075326.mp4
+
