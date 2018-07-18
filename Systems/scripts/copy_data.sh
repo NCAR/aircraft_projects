8b73e934878d520b98abb808772e7b4570e0c911
Taylor Thomas <taylort@ucar.edu>
2018-07-18 17:41:12 +0000

    update to copy_data.sh to reduce case sensitivity of user input
    svn path=/; revision=9133

--- Systems/scripts/copy_data.sh
+++ Systems/scripts/copy_data.sh
@@ -3,15 +3,12 @@
 # script to copy .ads files from aircraft to transfer media for a given flight
 # after connecting removable drive, run script to transfer file(s)
 ###----------------------------------------------------------------------------
-
 # assign list of parameters for transferring data
 PROJECT="WECAN"
 
-DATA_LOCATION="/var/r1/$PROJECT/"
-# DATA_LOCATION="/scr/tmp/taylort/copy_data_tests/WECAN"
+DATA_LOCATION="/var/r1/$PROJECT"
 
 TRANSFER_MEDIA="/run/media/ads/*/$PROJECT"
-# TRANSFER_MEDIA="/scr/tmp/taylort/copy_data_tests/8845_TEST_MEDIA_8943"
 
 echo "Enter flight to copy from $PROJECT e.g. rf01 or ff03:"
 read FLIGHT
@@ -28,24 +25,23 @@ else
 fi
 
 echo "Do you have a removable drive connected?"
-echo "Please type Y and press enter to confirm. Anything else and enter will stop script."
+echo "Please type Y or y and press enter to confirm. Anything else and enter will stop script."
 read DRIVE_CONNECTION
-if [ $DRIVE_CONNECTION == "Y" ]; then
+if [ $DRIVE_CONNECTION == "Y" ] || [ $DRIVE_CONNECTION == "y" ]; then
    echo "You entered $DRIVE_CONNECTION, which means you have a drive connected.";
-   rsync -av $DATA_LOCATION/*$FLIGHT* $TRANSFER_MEDIA
+   rsync -cav $DATA_LOCATION/*$FLIGHT* $TRANSFER_MEDIA
    EXIT="$?"
    echo "rsync exit status: $EXIT"
    if [ "$EXIT" -eq 0 ]; then
       #umount $TRANSFER_MEDIA;
       echo "Copy of .ads file(s) for $PROJECT$FLIGHT SUCCESSFUL."
-      echo "OK to remove drive."
+      echo "You can now safely remove the drive by right clicking the desktop icon."
    elif [ "$EXIT" -gt 0 ]; then
       echo "Copy of .ads file(s) for $PROJECT$FLIGHT UNSUCCESSFUL."
       echo "Check files under /var/r1/$PROJECT and try again."
    else
       echo "rsync error"
    fi
-
 else
    echo "You don't have a drive connected. Stopping script. Connect a removable drive and restart script."
 fi
