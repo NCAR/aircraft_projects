7853890a1b109e43d4ec2a9359c84870d4578ef0
Janine Aquino <janine@ucar.edu>
2018-07-25 21:15:22 +0000

    Add SRT and ICARTT processing. A little documentation.
    svn path=/; revision=5531

--- WECAN/C130_N130AR/fieldProc_setup.py
+++ WECAN/C130_N130AR/fieldProc_setup.py
@@ -9,32 +9,34 @@
 # This script only copies ADS, LRT, and KML back to Boulder (and QC plots
 # if requested). If you need something else, talk to an RAF SE.
 
-# Data dir to run on gstation
-rstudio_dir =    '/home/ads/RStudio/'
+# Plots - set path to RStudio dir
+rstudio_dir =    '/home/ads/RStudio/'  # Data dir to run on gstation
+#rstudio_dir =    '/h/eol/ads/RStudio/' # Data dir to run on EOL servers
 
-# Data dir to run on EOL servers
-#rstudio_dir =    '/h/eol/ads/RStudio/'
-
+# Software - set locations of needed software
 translate2ds = '/opt/local/bin/translate2ds '
 
 # Products - set to true if you want 'em
 nc2asc = False # Generate ICARTT
-nc2iwg = False # Generate IWG1 packet
+nc2iwg =  True # Generate IWG1 packet
 catalog = True # Send QC plots to field catalog
-HRT =    True  # Generate HRT .nc file
+HRT =     True # Generate HRT .nc file
+SRT =     True # Generate SRT .nc file
+
 # If processing was already done, and now someone wants HRT data,
 # to avoid reprocessing LRT, regenerating plots, etc, set HRTonly to 
 # True
 HRTonly = False
+
 
 ### NAS stuff ###
 # Do we have local SWIG RAID storage.
 NAS =     True
 # Does NAS have a permanent mount?
 NAS_permanent_mount = False
-
 nas_url =        '192.168.1.30:/data'
 nas_mnt_pt =     '/mnt/Data'
+
 
 ### remote FTP setup (if no NAS) ###
 #ftp_site =       'data.eol.ucar.edu'
