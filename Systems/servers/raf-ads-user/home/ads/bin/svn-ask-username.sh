607fe821a30199bcc06743afc444d6b40c65ab6f
Chris Webster <cjw@ucar.edu>
2018-08-12 17:43:12 +0000

    Remove. Now in github/ncar/aircraft_rpms
    svn path=/; revision=9172

--- Systems/servers/raf-ads-user/home/ads/bin/svn-ask-username.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# echo "nargs=$#"
-if [[ `id -un` == ads && ("$1" == ci || "$1" == commit) ]] && ! echo "$@" | fgrep -q -- --username; then
-	read -p "enter --username for svn checkin: " uname
-	\svn --username $uname "$@"
-else
-	\svn "$@"
-fi
