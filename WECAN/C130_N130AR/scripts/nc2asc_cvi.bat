8a375bbad86fd627d9108d9bf73366d57a7cb6d2
Chris Webster <cjw@ucar.edu>
2018-12-13 22:25:12 +0000

    CVI ICARTT batch file.
    svn path=/; revision=5667

---
+++ WECAN/C130_N130AR/scripts/nc2asc_cvi.bat
@@ -0,0 +1,17 @@
+hd= ICARTT
+avg= 1
+dt= NoDate
+tm= SecOfDay
+sp= Comma
+fv= FillValue
+ti= Full
+
+var=CVINLET
+var=CVFXFLOWS
+var=CVFX5
+var=CVFX6
+var=CVFX7
+var=CVCWCC
+var=CVRAD
+var=CVCFACT
+var=CONCU_CVIU
