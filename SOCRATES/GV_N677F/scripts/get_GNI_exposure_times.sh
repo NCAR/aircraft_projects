43d578c7022f75418b2e0b6c46b72ae600c38246
Janine Aquino <janine@ucar.edu>
2019-02-15 22:44:09 +0000

    Remove creation of empty files one dir up. No idea why I had that in there...
    svn path=/; revision=5680

--- SOCRATES/GV_N677F/scripts/get_GNI_exposure_times.sh
+++ SOCRATES/GV_N677F/scripts/get_GNI_exposure_times.sh
@@ -5,9 +5,9 @@ rm *exposure_times
 
 foreach dir ([RTF]F??_*)
     echo $dir
-    touch ../$dir.exposure_times
     foreach file ($dir/gni_server*log)
         echo $file
         /net/work/dev/jaa/gni/scripts/logs/get_exposure_times $file >> $dir.exposure_times
+        #/h/eol/cdewerd/code/gni/scripts/logs/get_exposure_times $file >> $dir.exposure_times
     end
 end
