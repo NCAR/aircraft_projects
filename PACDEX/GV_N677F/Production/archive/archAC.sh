1adaa8d6291411feaddeac15f0d3afb82c153762
Janine Aquino <janine@ucar.edu>
2019-09-17 21:15:25 +0000

    Add LRT versions
    svn path=/; revision=5950

--- PACDEX/GV_N677F/Production/archive/archAC.sh
+++ PACDEX/GV_N677F/Production/archive/archAC.sh
@@ -7,7 +7,14 @@
 ##   Project   #
 ################
 set PROJECT = "PACDEX"
+set YEAR = 2007
+
+
 #echo "Make sure netCDF files have been reordered before archiving!!!"
 #
 #### PMS2D
-/net/work/bin/scripts/mass_store/archAC/archAC.py HARP -r /scr/raf/Raw_Data/$PROJECT/HARP_spectral_irradiance sav EOL
+#/net/work/bin/scripts/mass_store/archAC/archAC.py HARP -r /scr/raf/Raw_Data/$PROJECT/HARP_spectral_irradiance sav EOL
+
+## LRT
+#/net/work/bin/scripts/mass_store/archAC/archAC.py LRT/Version1 /scr/raf/Prod_Data/$PROJECT/Version1 .nc /FS/EOL/$YEAR taylort@ucar.edu
+/net/work/bin/scripts/mass_store/archAC/archAC.py LRT/Version3 /scr/raf/Prod_Data/$PROJECT/Version3 .nc /FS/EOL/$YEAR taylort@ucar.edu
