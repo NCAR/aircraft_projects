d591f2132297ff31043b2a41caf93187ae0dbd2f
Janine Aquino <janine@ucar.edu>
2019-09-17 19:36:06 +0000

    ICARTT processing scripts
    svn path=/; revision=5914

--- DC3/GV_N677F/scripts/create_icartt.sh
+++ DC3/GV_N677F/scripts/create_icartt.sh
@@ -6,27 +6,27 @@ set nc2asc = "/opt/local/bin/nc2asc"
 #/opt/local/bin/nc2asc -b RAF-AEROSOL_template -i DC3rf01.nc -o DC3-RAF-AEROSOL_GV_20120518_R1.ICT
 #/opt/local/bin/nc2asc -b RAF-NAV_template -i DC3rf01.nc -o DC3-RAF-NAV_GV_20120518_R1.ICT
 
-foreach type (`cat types`)
-${nc2asc} -b RAF-${type}_template -i ../DC3rf01FZ.nc -o DC3-RAF-${type}_GV_20120518_R2.ICT
-${nc2asc} -b RAF-${type}_template -i ../DC3rf02FZ.nc -o DC3-RAF-${type}_GV_20120519_R2.ICT
-${nc2asc} -b RAF-${type}_template -i ../DC3rf03FZ.nc -o DC3-RAF-${type}_GV_20120521_R2.ICT
-${nc2asc} -b RAF-${type}_template -i ../DC3rf04FZ.nc -o DC3-RAF-${type}_GV_20120525_R2.ICT
-${nc2asc} -b RAF-${type}_template -i ../DC3rf05FZ.nc -o DC3-RAF-${type}_GV_20120526_R2.ICT
-${nc2asc} -b RAF-${type}_template -i ../DC3rf06FZ.nc -o DC3-RAF-${type}_GV_20120529_R2.ICT
-${nc2asc} -b RAF-${type}_template -i ../DC3rf07FZ.nc -o DC3-RAF-${type}_GV_20120530_R2.ICT
-${nc2asc} -b RAF-${type}_template -i ../DC3rf08FZ.nc -o DC3-RAF-${type}_GV_20120601_R2.ICT
-${nc2asc} -b RAF-${type}_template -i ../DC3rf09FZ.nc -o DC3-RAF-${type}_GV_20120605_R2.ICT
-${nc2asc} -b RAF-${type}_template -i ../DC3rf10FZ.nc -o DC3-RAF-${type}_GV_20120606_R2.ICT
-${nc2asc} -b RAF-${type}_template -i ../DC3rf11FZ.nc -o DC3-RAF-${type}_GV_20120607_R2.ICT
-${nc2asc} -b RAF-${type}_template -i ../DC3rf12FZ.nc -o DC3-RAF-${type}_GV_20120611_R2.ICT
-${nc2asc} -b RAF-${type}_template -i ../DC3rf13FZ.nc -o DC3-RAF-${type}_GV_20120615_R2.ICT
-${nc2asc} -b RAF-${type}_template -i ../DC3rf14FZ.nc -o DC3-RAF-${type}_GV_20120616_R2.ICT
-${nc2asc} -b RAF-${type}_template -i ../DC3rf15FZ.nc -o DC3-RAF-${type}_GV_20120617_R2.ICT
-${nc2asc} -b RAF-${type}_template -i ../DC3rf16FZ.nc -o DC3-RAF-${type}_GV_20120621_R2.ICT
-${nc2asc} -b RAF-${type}_template -i ../DC3rf17FZ.nc -o DC3-RAF-${type}_GV_20120622_R2.ICT
-${nc2asc} -b RAF-${type}_template -i ../DC3rf18FZ.nc -o DC3-RAF-${type}_GV_20120623_R2.ICT
-${nc2asc} -b RAF-${type}_template -i ../DC3rf19FZ.nc -o DC3-RAF-${type}_GV_20120625_R2.ICT
-${nc2asc} -b RAF-${type}_template -i ../DC3rf20FZ.nc -o DC3-RAF-${type}_GV_20120627_R2.ICT
-${nc2asc} -b RAF-${type}_template -i ../DC3rf21FZ.nc -o DC3-RAF-${type}_GV_20120628_R2.ICT
-${nc2asc} -b RAF-${type}_template -i ../DC3rf22FZ.nc -o DC3-RAF-${type}_GV_20120630_R2.ICT
+foreach type (`cat $PROJ_DIR/DC3/GV_N677F/scripts/types`)
+${nc2asc} -b $PROJ_DIR/DC3/GV_N677F/scripts/RAF-${type}_template -i ../DC3rf01FZ.nc -o DC3-RAF-${type}_GV_20120518_R3.ICT
+${nc2asc} -b $PROJ_DIR/DC3/GV_N677F/scripts/RAF-${type}_template -i ../DC3rf02FZ.nc -o DC3-RAF-${type}_GV_20120519_R3.ICT
+${nc2asc} -b $PROJ_DIR/DC3/GV_N677F/scripts/RAF-${type}_template -i ../DC3rf03FZ.nc -o DC3-RAF-${type}_GV_20120521_R3.ICT
+${nc2asc} -b $PROJ_DIR/DC3/GV_N677F/scripts/RAF-${type}_template -i ../DC3rf04FZ.nc -o DC3-RAF-${type}_GV_20120525_R3.ICT
+${nc2asc} -b $PROJ_DIR/DC3/GV_N677F/scripts/RAF-${type}_template -i ../DC3rf05FZ.nc -o DC3-RAF-${type}_GV_20120526_R3.ICT
+${nc2asc} -b $PROJ_DIR/DC3/GV_N677F/scripts/RAF-${type}_template -i ../DC3rf06FZ.nc -o DC3-RAF-${type}_GV_20120529_R3.ICT
+${nc2asc} -b $PROJ_DIR/DC3/GV_N677F/scripts/RAF-${type}_template -i ../DC3rf07FZ.nc -o DC3-RAF-${type}_GV_20120530_R3.ICT
+${nc2asc} -b $PROJ_DIR/DC3/GV_N677F/scripts/RAF-${type}_template -i ../DC3rf08FZ.nc -o DC3-RAF-${type}_GV_20120601_R3.ICT
+${nc2asc} -b $PROJ_DIR/DC3/GV_N677F/scripts/RAF-${type}_template -i ../DC3rf09FZ.nc -o DC3-RAF-${type}_GV_20120605_R3.ICT
+${nc2asc} -b $PROJ_DIR/DC3/GV_N677F/scripts/RAF-${type}_template -i ../DC3rf10FZ.nc -o DC3-RAF-${type}_GV_20120606_R3.ICT
+${nc2asc} -b $PROJ_DIR/DC3/GV_N677F/scripts/RAF-${type}_template -i ../DC3rf11FZ.nc -o DC3-RAF-${type}_GV_20120607_R3.ICT
+${nc2asc} -b $PROJ_DIR/DC3/GV_N677F/scripts/RAF-${type}_template -i ../DC3rf12FZ.nc -o DC3-RAF-${type}_GV_20120611_R3.ICT
+${nc2asc} -b $PROJ_DIR/DC3/GV_N677F/scripts/RAF-${type}_template -i ../DC3rf13FZ.nc -o DC3-RAF-${type}_GV_20120615_R3.ICT
+${nc2asc} -b $PROJ_DIR/DC3/GV_N677F/scripts/RAF-${type}_template -i ../DC3rf14FZ.nc -o DC3-RAF-${type}_GV_20120616_R3.ICT
+${nc2asc} -b $PROJ_DIR/DC3/GV_N677F/scripts/RAF-${type}_template -i ../DC3rf15FZ.nc -o DC3-RAF-${type}_GV_20120617_R3.ICT
+${nc2asc} -b $PROJ_DIR/DC3/GV_N677F/scripts/RAF-${type}_template -i ../DC3rf16FZ.nc -o DC3-RAF-${type}_GV_20120621_R3.ICT
+${nc2asc} -b $PROJ_DIR/DC3/GV_N677F/scripts/RAF-${type}_template -i ../DC3rf17FZ.nc -o DC3-RAF-${type}_GV_20120622_R3.ICT
+${nc2asc} -b $PROJ_DIR/DC3/GV_N677F/scripts/RAF-${type}_template -i ../DC3rf18FZ.nc -o DC3-RAF-${type}_GV_20120623_R3.ICT
+${nc2asc} -b $PROJ_DIR/DC3/GV_N677F/scripts/RAF-${type}_template -i ../DC3rf19FZ.nc -o DC3-RAF-${type}_GV_20120625_R3.ICT
+${nc2asc} -b $PROJ_DIR/DC3/GV_N677F/scripts/RAF-${type}_template -i ../DC3rf20FZ.nc -o DC3-RAF-${type}_GV_20120627_R3.ICT
+${nc2asc} -b $PROJ_DIR/DC3/GV_N677F/scripts/RAF-${type}_template -i ../DC3rf21FZ.nc -o DC3-RAF-${type}_GV_20120628_R3.ICT
+${nc2asc} -b $PROJ_DIR/DC3/GV_N677F/scripts/RAF-${type}_template -i ../DC3rf22FZ.nc -o DC3-RAF-${type}_GV_20120630_R3.ICT
 end
--- DC3/GV_N677F/scripts/update_comments.pl
+++ DC3/GV_N677F/scripts/update_comments.pl
@@ -13,20 +13,20 @@ foreach $file (`ls *ICT`) {
 
 	# Add new revision numbers to output here. Do NOT remove previous
 	# numbers. Comments are cumulative.
-	elsif (m/^REVISION:/) {print OUTFILE "REVISION:R2; R1; R0\n";}
+	elsif (m/^REVISION:/) {print OUTFILE "REVISION:R3; R2; R1; R0\n";}
 
 	# Add new revision number followed by text comment here.
-	elsif (m/^R0: Field Data$/) {print OUTFILE "R2: The NCAR/EOL Research Aviation Facility has recently completed a review and upgrade of its existing algorithms for data processing to incorporate recent improvements that have become available for specific instruments. For details, please see the Appendix to DC3 Project Manager’s Report: Reprocessing\nR1: Reprocess and release data to adjust for modified calibrations in the VCSEL data which are used as the reference hygrometer for all derived humidity variables in this dataset.\nR0: Final Data\n";}
+	elsif (m/^R0: Field Data$/) {print OUTFILE "R3: This update contains corrected 2DC data (CONC2DC[AR]_LWOI, DBAR2DC[AR]_LWOI, and PLWC2DC[AR]_LWOI). It was discovered that all particles in a single buffer got time-stamped with the data-system time tag of the end off the buffer. So for concentrations above 5-10/L, or when in heavy cloud, the system writes multiple buffers per second so the time-stamp will be very close to accurate. But when a buffer spans multiple seconds, all the particles were being binned to the second of the end time-stamp for the buffer. This has now been corrected so data is spread across the time-period of the buffer correctly. This version also uses exact circle fitting for measuring particle size and limits accepted particles to center-in images.\nR2: The NCAR/EOL Research Aviation Facility has recently completed a review and upgrade of its existing algorithms for data processing to incorporate recent improvements that have become available for specific instruments. For details, please see the Appendix to DC3 Project Manager’s Report: Reprocessing\nR1: Reprocess and release data to adjust for modified calibrations in the VCSEL data which are used as the reference hygrometer for all derived humidity variables in this dataset.\nR0: Final Data\n";}
 
 	# If highest revision is R2, then add two to header_lines
 	elsif (m/, 1001$/) {
 	    $header_lines = $_;
 	    $header_lines =~ s/, 1001//;
-	    $header_lines = $header_lines+2;
+	    $header_lines = $header_lines+3;
 	    print OUTFILE "$header_lines, 1001\n";
 
 	# If highest revision is R2, then add two to special comment count
-	} elsif (m/^18$/) {print OUTFILE "20\n";}
+	} elsif (m/^18$/) {print OUTFILE "21\n";}
 
 	else {print OUTFILE;}
     }
