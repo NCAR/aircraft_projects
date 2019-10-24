6695c5b083a19749540603dedf054146573e190d
Taylor Thomas <taylort@ucar.edu>
2019-10-24 16:16:07 +0000

    updates to movie processing script
    svn path=/; revision=5991

--- WECAN/C130_N130AR/scripts/createMovies.sh
+++ WECAN/C130_N130AR/scripts/createMovies.sh
@@ -1,22 +1,22 @@
 #/bin/csh
-nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf01
-nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf02
-nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf03
-nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf04
-nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf05
-nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf06
-nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf07
-nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf08
-nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf09
-nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf10
-nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf11
-nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf12
-nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf13
-nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf14
-nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf15
-nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf16
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf01
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf02
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf03
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf04
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf05
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf06
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf07
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf08
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf09
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf10
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf11
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf12
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf13
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf14
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf15
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf16
 nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf17
-nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf18
-nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf19
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf18
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl WECAN.paramfile rf19
 
 
