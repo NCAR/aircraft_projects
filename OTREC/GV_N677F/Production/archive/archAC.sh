408f306a7d2aec9d0487c1804e765793bd71f421
Taylor Thomas <taylort@ucar.edu>
2020-01-14 22:52:08 +0000

    preliminary movie processing and dataset archiving
    svn path=/; revision=6029

--- OTREC/GV_N677F/Production/archive/archAC.sh
+++ OTREC/GV_N677F/Production/archive/archAC.sh
@@ -8,11 +8,11 @@
 ###############
 setenv PROJECT OTREC
 setenv YEAR 2019
-### ADS 
+### ADS
 #/net/work/bin/scripts/mass_store/archAC/archAC.py ADS/maint_days /scr/raf/Raw_Data/$PROJECT/maint_days ads FS/EOL/$YEAR taylort@ucar.edu
 
 ### CAMERA
-#/net/work/bin/scripts/mass_store/archAC/archAC.py CAMERA -r /scr/raf/Raw_Data/$PROJECT/camera_images jpg FS/EOL/$YEAR taylort@ucar.edu
+/net/work/bin/scripts/mass_store/archAC/archAC.py CAMERA -r /scr/raf/Raw_Data/$PROJECT/camera_images/flight_number_rf10 jpg FS/EOL/$YEAR taylort@ucar.edu
 
 ########################## Preliminary Data Files #############################
 ### Preliminary LRT
@@ -58,7 +58,7 @@ setenv YEAR 2019
 #/net/work/bin/scripts/mass_store/archAC/archAC.py ICARTT /scr/raf/Prod_Data/$PROJECT/ICARTT_format ict FS/EOL/$YEAR
 
 ### PMS2D
-/net/work/bin/scripts/mass_store/archAC/archAC.py PMS2D /scr/raf/Raw_Data/$PROJECT/PMS2D 2d FS/EOL/$YEAR taylort@ucar.edu
+#/net/work/bin/scripts/mass_store/archAC/archAC.py PMS2D /scr/raf/Raw_Data/$PROJECT/PMS2D 2d FS/EOL/$YEAR taylort@ucar.edu
 
 ### KML
 #/net/work/bin/scripts/mass_store/archAC/archAC.py KML /scr/raf/Prod_Data/$PROJECT kml FS/EOL/$YEAR
--- OTREC/GV_N677F/scripts/filterImages.sh
+++ OTREC/GV_N677F/scripts/filterImages.sh
@@ -1,6 +1,6 @@
 #!/bin/csh
-/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/OTREC/camera_images/flight_number_rf08/forward/" "-proj:OTREC" "-s:15"
-/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/OTREC/camera_images/flight_number_rf08/down/" "-proj:OTREC" "-s:15"
-/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/OTREC/camera_images/flight_number_rf08/right/" "-proj:OTREC" "-s:15"
-/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/OTREC/camera_images/flight_number_rf08/left/" "-proj:OTREC" "-s:15"
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/OTREC/camera_images/flight_number_rf10/forward/" "-proj:OTREC" "-s:15"
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/OTREC/camera_images/flight_number_rf10/down/" "-proj:OTREC" "-s:15"
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/OTREC/camera_images/flight_number_rf10/right/" "-proj:OTREC" "-s:15"
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/OTREC/camera_images/flight_number_rf10/left/" "-proj:OTREC" "-s:15"
 
