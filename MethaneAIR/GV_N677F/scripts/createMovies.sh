e6868e990a4fa89660f70f6d4c2e3bdd9dcaceac
Taylor Thomas <taylort@ucar.edu>
2020-01-14 22:54:05 +0000

    phase 1 preliminary movie and dataset archiving
    svn path=/; revision=6030

--- MethaneAIR/GV_N677F/scripts/createMovies.sh
+++ MethaneAIR/GV_N677F/scripts/createMovies.sh
@@ -1,4 +1,3 @@
 #!/bin/csh
-nice +19 /net/work/bin/converters/createMovies/combineCameras.pl PROJECT.paramfile rf01
-nice +19 /net/work/bin/converters/createMovies/combineCameras.pl PROJECT.paramfile rf02
-# ...
+#nice +19 /net/work/bin/converters/createMovies/combineCameras.pl MethaneAIR.paramfile rf01
+nice +19 /net/work/bin/converters/createMovies/combineCameras.pl MethaneAIR.paramfile rf02
--- MethaneAIR/GV_N677F/scripts/filterImages.sh
+++ MethaneAIR/GV_N677F/scripts/filterImages.sh
@@ -1,3 +1,5 @@
 #!/bin/csh
-/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/XXXPROJECTXXX/camera_images/flight_number_rf01/forward/" "-proj:XXXPROJECTXXX" "-s:15"
-/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/XXXPROJECTXXX/camera_images/flight_number_rf02/forward/" "-proj:XXXPROJECTXXX" "-s:15"
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/MethaneAIR/camera_images/flight_number_rf01/forward/" "-proj:MethaneAIR" "-s:15"
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/MethaneAIR/camera_images/flight_number_rf01/down/" "-proj:MethaneAIR" "-s:15"
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/MethaneAIR/camera_images/flight_number_rf02/forward/" "-proj:MethaneAIR" "-s:15"
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/MethaneAIR/camera_images/flight_number_rf02/down/" "-proj:MethaneAIR" "-s:15"
