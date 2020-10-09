aebbe3d9482e10192d18053d483b0559d3471648
Janine Aquino <janine@ucar.edu>
2020-10-09 13:42:33 -0600

    initialize project

---
+++ WCR-TEST/C130_N130AR/scripts/createMovies.sh
@@ -0,0 +1,4 @@
+#!/bin/csh
+nice +19 /net/work/bin/converters/createMovies/combineCameras.pl PROJECT.paramfile rf01
+nice +19 /net/work/bin/converters/createMovies/combineCameras.pl PROJECT.paramfile rf02
+# ...
