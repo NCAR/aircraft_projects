ea9449595e416d8b193ddecc614477af23ab8148
Catherine Dewerd <cdewerd@ucar.edu>
2021-06-07 13:47:42 -0600

    Move fix to generic location

--- scripts/copy_data.sh
+++ scripts/copy_data.sh
@@ -4,7 +4,7 @@
 # after connecting removable drive, run script to transfer file(s)
 ###----------------------------------------------------------------------------
 # assign list of parameters for transferring data
-PROJECT="WECAN"
+PROJECT="SPICULE"
 
 DATA_LOCATION="/var/r1/$PROJECT"
 
@@ -28,21 +28,22 @@ echo "Do you have a removable drive connected?"
 echo "Please type Y or y and press enter to confirm. Anything else and enter will stop script."
 read DRIVE_CONNECTION
 if [ $DRIVE_CONNECTION == "Y" ] || [ $DRIVE_CONNECTION == "y" ]; then
+   mkdir -P $TRANSFER_MEDIA/$PROJECT
    echo "You entered $DRIVE_CONNECTION, which means you have a drive connected.";
-   rsync -cav $DATA_LOCATION/*$FLIGHT* $TRANSFER_MEDIA/$PROJECT
+   rsync -cavP --no-perms  $DATA_LOCATION/*$FLIGHT* $TRANSFER_MEDIA/$PROJECT
    EXIT="$?"
    echo "rsync exit status: $EXIT"
    if [ "$EXIT" -eq 0 ]; then
       umount $TRANSFER_MEDIA;
       echo "Copy of .ads file(s) for $PROJECT$FLIGHT SUCCESSFUL."
       echo "When terminal closes you can safely remove the drive by right-clicking the desktop icon."
-      sleep 8
+      sleep 80
    elif [ "$EXIT" -gt 0 ]; then
       echo "Copy of .ads file(s) for $PROJECT$FLIGHT UNSUCCESSFUL."
       echo "Check files under /var/r1/$PROJECT and try again."
    else
       echo "rsync error"
-      sleep 8
+      sleep 80
    fi
 else
    echo "You don't have a drive connected. Stopping script. Connect a removable drive and restart script."
