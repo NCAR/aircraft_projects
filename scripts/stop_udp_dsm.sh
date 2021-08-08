9d1505d7fd40c8ee9ba3fa55c586e07bce99908f
Catherine dewerd <cdewerd@gmail.com>
2021-08-08 10:22:18 -0600

    Add script for udpDSM restart, Add dsm.pid removal in ensure_running

---
+++ scripts/stop_udp_dsm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Start local dsm process
+# Shutdown any existing process
+if  test -f "/tmp/run/nidas/dsm.pid" ; then
+    pkill -F /tmp/run/nidas/dsm.pid;
+    for (( i = 0; i < 10; i++ )); do
+        pgrep $(</tmp/run/nidas/dsm.pid) || break;
+        sleep 5
+        pkill -F /tmp/run/nidas/dsm.pid;
+    rm /tmp/run/nidas/dsm.pid
+    done
+fi
+#dsm "$xmlchoice"
