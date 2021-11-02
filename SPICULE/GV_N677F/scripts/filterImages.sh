43f16df49424d0f1adbafba4d4264fcdee7bcda7
tthomas88 <taylort@ucar.edu>
2021-11-02 16:23:35 -0600

    dir cleanup before SPICULE run_all

--- SPICULE/GV_N677F/scripts/filterImages.sh
+++ SPICULE/GV_N677F/scripts/filterImages.sh
@@ -1,3 +1,9 @@
 #!/bin/csh
-/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/XXXPROJECTXXX/camera_images/flight_number_rf01/forward/" "-proj:XXXPROJECTXXX" "-s:15"
-/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/XXXPROJECTXXX/camera_images/flight_number_rf02/forward/" "-proj:XXXPROJECTXXX" "-s:15"
+#/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/SPICULE/camera_images/flight_number_rf01/forward/" "-proj:SPICULE" "-s:15"
+#/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/SPICULE/camera_images/flight_number_rf04/forward/" "-proj:SPICULE" "-s:15"
+#/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/SPICULE/camera_images/flight_number_rf05/forward/" "-proj:SPICULE" "-s:15"
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/SPICULE/camera_images/flight_number_rf06/forward/" "-proj:SPICULE" "-s:15"
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/SPICULE/camera_images/flight_number_rf07/forward/" "-proj:SPICULE" "-s:15"
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/SPICULE/camera_images/flight_number_rf08/forward/" "-proj:SPICULE" "-s:15"
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/SPICULE/camera_images/flight_number_rf09/forward/" "-proj:SPICULE" "-s:15"
+/net/work/bin/converters/ImageFilter/Image_Filter.pl "/scr/raf/Raw_Data/SPICULE/camera_images/flight_number_rf10/forward/" "-proj:SPICULE" "-s:15"
