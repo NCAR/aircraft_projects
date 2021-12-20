f435e048b99009570248fa5c4d910d35ad3af8c9
Janine Aquino <janine@ucar.edu>
2021-12-20 14:42:50 -0700

    camera processing scripts from SVN

--- scripts/camera/createMovie.py
+++ scripts/camera/createMovie.py
@@ -5,6 +5,6 @@ import os
 flight = raw_input('Input flight designation (e.g. tf01):')
 print flight
 
-command = "/home/local/raf/instruments/camera/createMovies/combineCameras.pl /home/local/projects/OTREC/GV_N677F/scripts/OTREC.paramfile " + flight
+command = "combineCameras.pl movieParamFile " + flight
 
 os.system(command)
