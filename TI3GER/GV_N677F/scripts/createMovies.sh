11c9ccbdb927b3926de63989c98b84e7108f15f9
tthomas88 <taylort@ucar.edu>
2022-05-09 11:26:46 -0600

    tikal TI3GER movie processing

--- TI3GER/GV_N677F/scripts/createMovies.sh
+++ TI3GER/GV_N677F/scripts/createMovies.sh
@@ -1,4 +1,4 @@
 #!/bin/csh
 
-nice +19 /net/jlocal/projects/scripts/camera/combineCameras.pl ../movieParamFile rf01
+nice +19 /net/jlocal/projects/scripts/camera/combineCameras.pl ../movieParamFile rf08
 
--- TI3GER/GV_N677F/scripts/filterImages.sh
+++ TI3GER/GV_N677F/scripts/filterImages.sh
@@ -1,6 +1,6 @@
 #! /bin/csh -f
 
-perl /net/jlocal/projects/scripts/camera/Image_Filter.pl "/scr/raf/Raw_Data/TI3GER/camera/flight_number_rf01/forward/" "-s:60" "-t:5i"
-perl /net/jlocal/projects/scripts/camera/Image_Filter.pl "/scr/raf/Raw_Data/TI3GER/camera/flight_number_rf01/left/" "-s:60" "-t:5i"
-perl /net/jlocal/projects/scripts/camera/Image_Filter.pl "/scr/raf/Raw_Data/TI3GER/camera/flight_number_rf01/right/" "-s:60" "-t:5i"
-perl /net/jlocal/projects/scripts/camera/Image_Filter.pl "/scr/raf/Raw_Data/TI3GER/camera/flight_number_rf01/down/" "-s:60" "-t:5i"
+perl /net/jlocal/projects/scripts/camera/Image_Filter.pl "/scr/raf/Raw_Data/TI3GER/camera/flight_number_rf08/forward/" "-s:60" "-t:5i"
+perl /net/jlocal/projects/scripts/camera/Image_Filter.pl "/scr/raf/Raw_Data/TI3GER/camera/flight_number_rf08/left/" "-s:60" "-t:5i"
+perl /net/jlocal/projects/scripts/camera/Image_Filter.pl "/scr/raf/Raw_Data/TI3GER/camera/flight_number_rf08/right/" "-s:60" "-t:5i"
+perl /net/jlocal/projects/scripts/camera/Image_Filter.pl "/scr/raf/Raw_Data/TI3GER/camera/flight_number_rf08/down/" "-s:60" "-t:5i"
