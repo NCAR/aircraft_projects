d956fa301e584ff2b4fb8ac4788d8e1476fce4ff
tthomas88 <taylort@ucar.edu>
2022-07-22 09:54:02 -0600

    Update to ACCLIP movie processing script configuration
    No down camera, so layout is forward camera at "North", left at
    "SouthWest" and right at "SouthEast"

--- ACCLIP/GV_N677F/scripts/createMovies.sh
+++ ACCLIP/GV_N677F/scripts/createMovies.sh
@@ -1,17 +1,2 @@
 #!/bin/bash
-
-./net/jlocal/projects/scripts/camera/combineCameras.pl ../movieParamFile rf01
-./net/jlocal/projects/scripts/camera/combineCameras.pl ../movieParamFile rf02
-./net/jlocal/projects/scripts/camera/combineCameras.pl ../movieParamFile rf03
-./net/jlocal/projects/scripts/camera/combineCameras.pl ../movieParamFile rf04
-./net/jlocal/projects/scripts/camera/combineCameras.pl ../movieParamFile rf05
-./net/jlocal/projects/scripts/camera/combineCameras.pl ../movieParamFile rf06
-./net/jlocal/projects/scripts/camera/combineCameras.pl ../movieParamFile rf07
-./net/jlocal/projects/scripts/camera/combineCameras.pl ../movieParamFile rf08
-./net/jlocal/projects/scripts/camera/combineCameras.pl ../movieParamFile rf09
-./net/jlocal/projects/scripts/camera/combineCameras.pl ../movieParamFile rf10
-./net/jlocal/projects/scripts/camera/combineCameras.pl ../movieParamFile rf11
-./net/jlocal/projects/scripts/camera/combineCameras.pl ../movieParamFile rf12
-./net/jlocal/projects/scripts/camera/combineCameras.pl ../movieParamFile rf13
-./net/jlocal/projects/scripts/camera/combineCameras.pl ../movieParamFile rf14
-./net/jlocal/projects/scripts/camera/combineCameras.pl ../movieParamFile rf15
+/net/jlocal/projects/scripts/camera/combineCameras.pl ../movieParamFile tf01
