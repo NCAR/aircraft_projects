2d154d692518c42db7bedd2ed874b870d77921f5
tthomas88 <taylort@ucar.edu>
2022-09-09 17:53:00 +0000

    Minor update to the maire gcp copy script.

--- MAIRE/GV_N677F/scripts/maire_gcp_transfer.py
+++ MAIRE/GV_N677F/scripts/maire_gcp_transfer.py
@@ -1,11 +1,15 @@
-#!/usr/bin/env python3
+#! /bin/python3
 
 #############################################################################
 # script to copy raw MAIR-E instrument data from RAF ground station
 # computer to MethaneSAT Google Cloud Platform (GCP) bucket.
 # can be run on either RAF station computer.
 #############################################################################
+
 import os
+import re
+import sys
+import string
 import smtplib
 from email.mime.text import MIMEText
 
@@ -17,18 +21,20 @@ bucket_url = "gs://maire_test"
 # Function to copy data
 def gcpCopy():
 
-        try:
-            os('time ionice -c 2 -n 7 gsutil -m cp -r  /home/data/MAIRE/' + bucket_url)
-            #os('time ionice -c 2 -n 7 gsutil -m cp -r /run/media/ads/' + drive_name + '/' + flight_date + ' ' + bucket_url)
-            msg = MIMEText('Data available at: ' + bucket_url )
-            msg['Subject'] = 'MAIR-E GCP Raw Instrument Data Transfer Complete'
-            msg['From'] = 'ads@groundstation'
-            msg['To'] = 'taylort@ucar.edu'
-            s = smtplib.SMTP('localhost')
-            s.sendmail('ads@groundstation','taylort@ucar.edu', msg.as_string())
-            s.quit()
-        except Exception as e:
-            print(e)
+    try:
+        command = 'time ionice -c 2 -n 7 gsutil -m cp -r  /home/data/MAIRE/ ' + bucket_url
+        os.system(command)
+        #os('time ionice -c 2 -n 7 gsutil -m cp -r /run/media/ads/' + drive_name + '/' + flight_date + ' ' + bucket_url)
+        msg = MIMEText('Data available at: ' + bucket_url)
+        print(msg)
+        msg['Subject'] = 'MAIR-E GCP Raw Instrument Data Transfer Complete'
+        msg['From'] = 'ads@groundstation'
+        msg['To'] = 'taylort@ucar.edu'
+        s = smtplib.SMTP('localhost')
+        s.sendmail('ads@groundstation','taylort@ucar.edu', msg.as_string())
+        s.quit()
+    except Exception as e:
+        print(e)
 
 # Main function
 def main():
