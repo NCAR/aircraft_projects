069d190519b962409eaf8bc7314ec6f905ebd9ed
Taylor Thomas <taylort@ucar.edu>
2023-09-06 11:03:30 -0600

    Turn ship ADS to False

--- APAR-FVT2023/C130_N130AR/scripts/fieldProc_setup.py
+++ APAR-FVT2023/C130_N130AR/scripts/fieldProc_setup.py
@@ -62,7 +62,7 @@ zip_ADS = False # Bzips the ads file independently of processed files
 # you can have both sendzipped and zip_ads set to True if you want
 
 # Do you want to transfer ADS file back to Boulder (is the connection good enough?)
-ship_ADS = True
+ship_ADS = False
 ship_all_ADS = False
 
 ### Instrument specific processing ###
