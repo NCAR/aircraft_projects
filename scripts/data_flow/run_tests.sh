8ee657b91512336ed8ba11cf19e0367b7ca0c42e
Janine Aquino <janine@ucar.edu>
2024-01-29 20:19:32 -0700

    Configure a single test to run. Remaining tests still need to be modified to use env vars to set paths

--- scripts/data_flow/run_tests.sh
+++ scripts/data_flow/run_tests.sh
@@ -1,9 +1,23 @@
 ###############################################################################
-#
-#
+# Shell script to call Python units tests. Configure environment (if not
+# running on a server/groundstation/etc) and call tests.
 #
 # Written in Python 3
 #
 # COPYRIGHT:   University Corporation for Atmospheric Research, 2020
 ###############################################################################
-python3 -m unittest discover -s test -v
+# If running someplace the standard RPMs aren't installed, such as on a desktop
+# MAC, configure a bunch of environment vars. These can also be used to
+# override local config.
+export PROJ_DIR="/Users/janine/dev/aircraft_projects"
+export PROJECT="CAESAR"
+export AIRCRAFT="C130_N130AR"
+export RAW_DATA_DIR="/Users/janine/dev/projects"
+export DATA_DIR="/Users/janine/dev/projects"
+export FTPUSER=username  # usually project name, lower case
+export FTPPWD=password
+export FTPDATADIR=EOL_data/RAF_data
+export FTPPARENTDIR=.
+# To run a single test use the -p option. To run all tests, omit -p
+python3 -m unittest discover -s test -v -p test_createConfigExt.py
+#python3 -m unittest discover -s test -v
