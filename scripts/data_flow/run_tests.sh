802bffd5ad2262afef9f0ec6902d5e49a160f532
Janine Aquino <janine@ucar.edu>
2024-01-30 20:47:56 -0700

    Get env vars out of here - use bashrc config instead.

--- scripts/data_flow/run_tests.sh
+++ scripts/data_flow/run_tests.sh
@@ -7,17 +7,18 @@
 # COPYRIGHT:   University Corporation for Atmospheric Research, 2020
 ###############################################################################
 # If running someplace the standard RPMs aren't installed, such as on a desktop
-# MAC, configure a bunch of environment vars. These can also be used to
-# override local config.
-export PROJ_DIR="/Users/janine/dev/aircraft_projects"
-export PROJECT="CAESAR"
-export AIRCRAFT="C130_N130AR"
-export RAW_DATA_DIR="/Users/janine/dev/projects"
-export DATA_DIR="/Users/janine/dev/projects"
-export FTPUSER=username  # usually project name, lower case
-export FTPPWD=password
-export FTPDATADIR=EOL_data/RAF_data
-export FTPPARENTDIR=.
+# MAC, be sure that the standard environment vars are configured through your
+# .bashrc setup.
+#
+# In .bashrc:
+#   if [[ -f ~/.Jeffco_only.bashrc ]]; then
+#       . ~/.Jeffco_only.bashrc
+#   fi
+#   source $HOME/ads3_environment.sh
+#   source $HOME/.ftpconfig
+# and copy .Jeffco_only.bashrc, ads3_environemtn.sh and .ftpconfig from a
+# server where the standard RPMs are installed.
+
 # To run a single test use the -p option. To run all tests, omit -p
 python3 -m unittest discover -s test -v -p test_createConfigExt.py
 #python3 -m unittest discover -s test -v
