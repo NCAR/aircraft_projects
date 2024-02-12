6bcfc0fe2896b58068244a05cde473a8c67de44e
Sara Runkel <145368282+sararunkel@users.noreply.github.com>
2024-02-12 16:40:10 -0700

    Refactpred test_createFileExt.py to use pytest instead of unittest and added multiple test parameters.

--- scripts/data_flow/test/test_createFileExt.py
+++ scripts/data_flow/test/test_createFileExt.py
@@ -1,26 +1,31 @@
-#! /usr/bin/python3
-
-import unittest
-import imp
+import pytest
 import sys
 import os
 sys.path.insert(0, os.environ['PROJ_DIR'] + '/' + os.environ['PROJECT'] + '/' + os.environ['AIRCRAFT'] + '/scripts')
-
 from fieldProc_setup import *
-
-class TEST_createFileExt(unittest.TestCase):
-
-    def setUp(self):
-        push_data = imp.load_source('push_data', os.environ['PROJ_DIR'] + '/' + 'scripts/data_flow/push_data.py')
-        self.fielddata = push_data.FieldData()
-        self.fielddata.createFileExt(HRT, SRT, ICARTT, IWG1, PMS2D, threeVCPI)
-
-    def tearDown(self):
-        pass
-
-    def test_createFileExt(self):
-        self.assertTrue(isinstance(self.fielddata.file_ext, dict))
+from unittest.mock import patch
+from collections import OrderedDict
+from const_tests import *
+sys.path.insert(0, os.environ['PROJ_DIR'] + '/scripts/data_flow')
+from push_data import FieldData
 
 
-if __name__ == '__main__':
-    unittest.main()
+@pytest.fixture
+def fielddata():
+    with (patch('os.environ.get', side_effect=mock_getenv), patch('builtins.input', side_effect=mock_input)):
+        return FieldData()
+
+ord_dict = OrderedDict([("ADS", "ads"), ("LRT", "nc"), ("KML", "kml")])
+caesar_dict=OrderedDict([("ADS", "ads"), ("LRT", "nc"), ("KML", "kml"),('PMS2D','2d')])
+@pytest.mark.parametrize(
+    "test_id, HRT, SRT, ICARTT, IWG1, PMS2D, threeVCPI, expected_output", [
+        # Test Case 1:
+        ('test1', False, False, False, False, False, False, ord_dict),
+        ('test2', HRT, SRT, ICARTT, IWG1, PMS2D, threeVCPI, caesar_dict),
+        #('test2', 'tf01', 'CAESAR', 'h','ict',False,False,'/Users/srunkel/dev/projects/CAESAR/CAESARtf01h.ict',False, None),
+        # ... more test cases with different file paths, flags, etc.
+    ]
+)
+def test_createFileExt(fielddata, test_id, HRT, SRT, ICARTT, IWG1, PMS2D, threeVCPI, expected_output):
+    result = fielddata.createFileExt(HRT, SRT, ICARTT, IWG1, PMS2D, threeVCPI)
+    assert result == expected_output
