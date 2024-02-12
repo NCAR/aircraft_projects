773e3b771c01c3fc1e3fd62150c4e997815c0248
Sara Runkel <145368282+sararunkel@users.noreply.github.com>
2024-02-12 15:04:18 -0700

    Refactored test_find_file.py and added two test cases to address multiple files and user inputs.

--- scripts/data_flow/test/test_find_file.py
+++ scripts/data_flow/test/test_find_file.py
@@ -1,4 +1,4 @@
-
+import fnmatch
 import sys
 import os
 
@@ -21,19 +21,31 @@ def fielddata():
         return FieldData()
 
 @pytest.mark.parametrize(
-    "test_id, FLIGHT, PROJECT, expected_datafile, expected_flag", [
+    "test_id, flight, project, filetype, fileext, flag, reprocess,  expected_datafile, expected_flag, user_inputs", [
         # Test Case 1:
-        ('test1', 'rf01', 'ACCLIPrf01', '/sample/path.nc', True),
+        ('test1', 'tf01', 'CAESAR', 'h','nc',True,True,'/Users/srunkel/dev/projects/CAESAR/ttCAESARtf01h.nc', True,['N','Y']),
+        ('test2', 'tf01', 'CAESAR', 'h','ict',False,False,'/Users/srunkel/dev/projects/CAESAR/CAESARtf01h.ict',False, None),
         # ... more test cases with different file paths, flags, etc.
     ]
 )
-def test_find_file(fielddata, test_id, FLIGHT, PROJECT, expected_datafile, expected_flag):
+def test_find_file(fielddata, test_id, flight, project, filetype, fileext, flag, reprocess, expected_datafile, expected_flag, user_inputs):
+    fake_file_list = [
+        '/Users/srunkel/dev/projects/CAESAR/CAESARtf01h.nc',
+        '/Users/srunkel/dev/projects/CAESAR/ttCAESARtf01h.nc',# Matches pattern in 'else' branch
+        '/Users/srunkel/dev/projects/CAESAR/CAESARtf01h.ict',# Matches pattern in 'if' branch
+        '/Users/srunkel/dev/projects/CAESAR/atf01h.ict'
+        # ... more fake files if needed
+    ]
+    #pattern = fielddata.data_dir + "*" + flight + filetype + '.' + fileext
+    def mock_input(prompt):
+        return user_inputs.pop(0) if user_inputs else ""
+    def mock_glob_side_effect(pattern):
+        return [file for file in fake_file_list if fnmatch.fnmatch(file, pattern)]
     # Mocking glob.glob (you can also use monkeypatch if preferred)
-    with patch('glob.glob') as mock_glob:
-        mock_glob.return_value = [expected_datafile]
+    with patch('glob.glob', side_effect=mock_glob_side_effect), patch('builtins.input', side_effect=mock_input):
 
         actual_flag, actual_datafile = fielddata.find_file(
-            fielddata.data_dir, FLIGHT, PROJECT, filetype, fileext, flag, reprocess
+            fielddata.data_dir, flight, project, filetype, fileext, flag, reprocess
         )
 
         assert actual_flag == expected_flag
