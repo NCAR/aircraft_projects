4c8fcfe41cd81e140f3315b151381870244e36fa
Sara Runkel <145368282+sararunkel@users.noreply.github.com>
2024-03-22 17:08:40 -0600

    Refactor _FTP.py

--- scripts/data_flow/push_data_reorg/_FTP.py
+++ scripts/data_flow/push_data_reorg/_FTP.py
@@ -1,120 +1,100 @@
 import os
 import sys
+import ftplib
 from scripts.data_flow.push_data_reorg._logging import *
 sys.path.insert(0, os.environ['PROJ_DIR'] + '/' + os.environ['PROJECT'] + '/' + os.environ['AIRCRAFT'] + '/scripts')
 from fieldProc_setup import user, password, DATA_DIR, RAW_DATA_DIR, dat_parent_dir, rdat_parent_dir, NAS, NAS_permanent_mount, nas_url, nas_mnt_pt, FTP, ftp_site, password, ftp_parent_dir, ftp_data_dir, ICARTT, IWG1, HRT, SRT, sendzipped, zip_ADS, ship_ADS, ship_all_ADS, PMS2D, threeVCPI, QA_notebook, catalog, rstudio_dir, translate2ds, datadump, GDRIVE, rclone_staging_dir
 
+def setup_ftp(self, status, file_ext, inst_dir, filename):
+    """Transfers files to an FTP server.
 
-def setup_FTP(self, data_dir, raw_dir, status, file_ext, inst_dir, filename):
-    '''No NAS this project, so put files to EOL server. Put
-    zipped files if they exist.
-    '''
+    Args:
+        data_dir (str): Base data directory (not directly used in this function).
+        status (dict): Dictionary to track file transfer status.
+        file_ext (dict): Dictionary of file extensions and their processing status.
+        inst_dir (dict): Dictionary of instrument directories.
+        filename (dict): Dictionary containing filenames for each instrument.
+    """
+
     try:
-        message = 'Opening FTP connection to: ' + ftp_site
-        log_and_print(message)
-        ftp = ftplib.FTP(ftp_site)
-        ftp.login(user, password)
-        print('')
-
+        self._connect_to_ftp()
     except ftplib.all_errors as e:
-        message = 'Error connecting to FTP site ' + ftp_site
-        log_and_print(message +'\n'+e, 'error')
+        log_and_print(f'Error connecting to FTP site {ftp_site}: {e}','error')
+        return
 
     print('Putting files to FTP site:')
-    print('')
 
-    # When ftping, put PMS2D file in PMS2D subdir
-    inst_dir['PMS2D'] = inst_dir['PMS2D'] + 'PMS2D/'
-
-    # If set in config file script will FTP all ads in rdat
-    # Keep this set to False unless you have time / bandwidth
-    if ship_all_ADS is True:
-        message = 'Starting ftp process for all available .ads files'
-        self.logger.info(message)
-        print(message)
-        for rawfilename in os.listdir(inst_dir['ADS']):
-            if rawfilename.endswith('.ads'):
-                try:
-                    os.chdir(inst_dir['ADS'])
-                    ftp.cwd('/' + ftp_data_dir + '/ADS')
-                    ftp.storbinary('STOR ' + rawfilename, open(rawfilename, 'rb'))
-                    status["ADS"]["stor"] = 'Yes-FTP'
-                    message = rawfilename + ' ftp successful!'
-                    log_and_print(message)
-                except Exception as e:
-                    message = rawfilename + ' not sent'
-                    log_and_print(message)
-                    log_and_print(e,'error')
-
+    if ship_all_ADS:
+        self._transfer_all_ads(status, inst_dir)
     else:
-        # Loop through requested file extensions to be copied to ftp area
-        for key in file_ext:
-            print('\n' + key + '\n')
+        self._transfer_selected_files(file_ext, inst_dir, filename)
 
-            if ship_ADS is False and key == 'ADS':
-                # Skip ads if requested in fieldProc_setup.py
-                continue
-            else:
-                try:
-                    os.chdir(inst_dir[key])
-                    print('Attempt to change to local dir ' + inst_dir[key])
-                except ftplib.all_errors as e:
-                    print('Could not change to local dir ' + inst_dir[key])
-                    log_and_print(e,'error')
-                    continue
-
-            if filename[key] != '':
-                print('Instrument file is ' + filename[key])
-                # Get instrument filename; used for error reporting
-                data_dir, file_name = os.path.split(filename[key])
-
-                try:
-                    print('Attempt to change to ftp dir /' + ftp_data_dir + '/' + key)
-                    ftp.cwd('/' + ftp_data_dir + '/' + key)
-                except ftplib.all_errors as e:
-                    print('Could not change to dir ' + ftp_data_dir + '/' + key)
-                    log_and_print(e,'error')
-                    # Attempt to create needed dir
-                    print('Attempt to create dir /' + ftp_data_dir + '/' + key)
-                    try:
-                        ftp.mkd('/' + ftp_data_dir + '/' + key)
-                    except Exception as e:
-                        print('Make dir /' + ftp_data_dir + '/' + key + ' failed')
-                        log_and_print(e,'error')
-                        continue
-                    # Try to change to dir again
-                    try:
-                        ftp.cwd('/' + ftp_data_dir + '/' + key)
-                    except Exception as e:
-                        print('Change dir to ' + ftp_data_dir + '/' + key + ' failed')
-                        log_and_print(e)
-                        continue
-
-            if file_name in ftp.nlst():
-                print('File ' + file_name + ' already exists on ftp server.')
-                print('File will not be transfered to ftp site')
-                print('To force transfer, delete file from ftp site and rerun in Ship mode')
-                continue
-
-            # Transfer files to FTP site
-            try:
-                print('Transferring file...')
-                file = open(file_name, 'rb')
-                print(ftp.storbinary('STOR ' + file_name, file))
-                file.close()
-                status[key]["stor"] = 'Yes-FTP'
-
-                print(datetime.datetime.now().time())
-                print('Finished putting data file')
-                print('')
-
-            except ftplib.all_errors as e:
-                print('Error writing ' + file_name + ' to ' + ftp_site + ':/' + ftp_data_dir + '/' + key)
-                log_and_print(e)
-                continue
-
-    ftp.quit()
-
+    self.ftp.quit()
     # Revert PMS2D ftp special case
     inst_dir['PMS2D'] = re.sub('PMS2D/', '',inst_dir['PMS2D'])
     print("After FTP: " + inst_dir['PMS2D'])
+
+
+def _connect_to_ftp(self):
+    message = f'Opening FTP connection to: {ftp_site}'
+    log_and_print(message)
+    self.ftp = ftplib.FTP(ftp_site)
+    self.ftp.login(user, password)
+
+
+def _transfer_all_ads(self, status, inst_dir):
+    # ... Implementation for transferring all ADS files ...
+    message = 'Starting ftp process for all available .ads files'
+    log_and_print(message)
+    for rawfilename in os.listdir(inst_dir['ADS']):
+        if rawfilename.endswith('.ads'):
+            try:
+                os.chdir(inst_dir['ADS'])
+                ftp.cwd(f'/{ftp_data_dir}/ADS')
+                ftp.storbinary(f'STOR {rawfilename}', open(rawfilename, 'rb'))
+                status["ADS"]["stor"] = 'Yes-FTP'
+                message = f'{rawfilename} ftp successful!'
+                log_and_print(message)
+            except Exception as e:
+                message = f'{rawfilename} not sent'
+                log_and_print(f'{message}/n{e}')
+                
+
+
+def _transfer_selected_files(self, file_ext, inst_dir, filename):
+    for key in file_ext:
+        if key == 'ADS' and not ship_ADS:
+            continue
+
+        if filename[key] == '':
+            continue
+
+        try:
+            self._create_ftp_dir_if_needed(key)
+            self._transfer_file(key, inst_dir[key], filename[key], status)
+        except (ftplib.all_errors, OSError) as e:
+            self.logger.error(f"Error processing {key}: {e}")
+
+
+def _create_ftp_dir_if_needed(self, instrument):
+    remote_dir = f'/{ftp_data_dir}/{instrument}'
+    try:
+        self.ftp.cwd(remote_dir)  # Check if exists
+    except ftplib.all_errors:
+        try:
+            self.ftp.mkd(remote_dir)  # Create if it doesn't
+            self.logger.info(f"Created FTP directory: {remote_dir}")
+        except ftplib.all_errors as e:
+            self.logger.error(f"Failed to create FTP directory {remote_dir}: {e}")
+
+
+def _transfer_file(self, instrument, local_dir, file_name, status):
+    remote_path = f'/{ftp_data_dir}/{instrument}/{file_name}'
+    if remote_path not in self.ftp.nlst():  # Check for existing file 
+        try:
+            with open(os.path.join(local_dir, file_name), 'rb') as file:
+                self.ftp.storbinary(f'STOR {remote_path}', file)
+            status[instrument]["stor"] = 'Yes-FTP'
+            print(f"Successfully transferred {file_name}")
+        except (ftplib.all_errors, OSError) as e:
+            self.logger.error(f"Error transferring {file_name}: {e}")
