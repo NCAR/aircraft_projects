04b799ed86598185eba296732358f9c225caf1dd
Sara Runkel <srunkel@mercury.eol.ucar.edu>
2024-05-16 14:53:27 -0600

    Fixed csh syntax and added project option.

--- scripts/camera/filterImages.sh
+++ scripts/camera/filterImages.sh
@@ -1,8 +1,9 @@
 #! /bin/csh -f
 
-FLIGHT="rf01"
+set FLIGHT=rf05
+set PROJECT=CAESAR
 
-perl ./Image_Filter.pl "/scr/raf/Raw_Data/<PROJECT>/camera_images/flight_number_$FLIGHT/forward/" "-s:60" "-t:5i"
-#perl ./Image_Filter.pl "/scr/raf/Raw_Data/<PROJECT>/camera_images/flight_number_$FLIGHT/left/" "-s:60" "-t:5i"
-#perl ./Image_Filter.pl "/scr/raf/Raw_Data/<PROJECT>/camera_images/flight_number_$FLIGHT/right/" "-s:60" "-t:5i"
-#perl ./Image_Filter.pl "/scr/raf/Raw_Data/<PROJECT>/camera_images/flight_number_$FLIGHT/down/" "-s:60" "-t:5i"
+#perl ./Image_Filter.pl "/scr/raf/Raw_Data/$PROJECT/camera_images/flight_number_$FLIGHT/forward/" "-s:60" "-t:5i" "-proj:$PROJECT"
+#perl ./Image_Filter.pl "/scr/raf/Raw_Data/$PROJECT/camera_images/flight_number_$FLIGHT/left/" "-s:60" "-t:5i" "-proj:$PROJECT"
+#perl ./Image_Filter.pl "/scr/raf/Raw_Data/$PROJECT/camera_images/flight_number_$FLIGHT/right/" "-s:60" "-t:5i" "-proj:$PROJECT"
+perl ./Image_Filter.pl "/scr/raf/Raw_Data/$PROJECT/camera_images/flight_number_$FLIGHT/down/" "-s:60" "-t:5i" "-proj:$PROJECT"
