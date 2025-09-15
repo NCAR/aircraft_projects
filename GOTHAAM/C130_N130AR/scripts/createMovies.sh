f6f0fa09d340df48fc75d8b5e9caeba177156875
sararunkel <srunkel@ucar.edu>
2025-09-15 09:25:18 -0600

    Update movie create scripts for gothaam.

--- GOTHAAM/C130_N130AR/scripts/createMovies.sh
+++ GOTHAAM/C130_N130AR/scripts/createMovies.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-FLIGHT="rf09"
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 flight1 flight2 flight3 ..."
+    echo "Example: $0 rf06 rf07 rf08"
+    exit 1
+fi
 
-/net/jlocal/projects/scripts/camera/combineCameras.pl ../movieParamFile $FLIGHT
+# Create template if it doesn't exist (one-time setup)
+if [ ! -f ../movieParamFile.template ]; then
+    cp ../movieParamFile ../movieParamFile.template
+fi
+
+for FLIGHT in "$@"; do
+    echo "Processing flight: $FLIGHT"
+
+    # Create movieParamFile from template, replacing rf00 with current flight
+    sed "s/rf00/$FLIGHT/g" ../movieParamFile.template > ../movieParamFile
+    
+    # Run the movie creation
+    /net/jlocal/projects/scripts/camera/combineCameras.pl ../movieParamFile $FLIGHT
+    
+    echo "Completed flight: $FLIGHT"
+done
--- scripts/camera/createMovies.sh
+++ scripts/camera/createMovies.sh
@@ -3,3 +3,4 @@
 FLIGHT="rf01"
 
 ./net/jlocal/projects/scripts/camera/combineCameras.pl ../movieParamFile $FLIGHT
+#TODO: Update createMovies.sh to be in only one location -- either project specific scripts folder or general scripts folder
