NCAR/aircraft_projects

commit 049fedb54190bf2a61c05f5e186ec7be99897367

Accept files with pp designation (pilot pro)

Configuration/scripts/archAC.py
@@ -651,7 +651,7 @@ if __name__ == "__main__":
                         (eyr,emo,edy,ehr,emn,esc,hoursearchstr)=archraf.parse_date(Efile)
                         # output tarfile name is like
                         # RF##.FWD.Sdate.Stime_etime.jpg.tar and tar.dir
-                        match = re.search("[RrTtFf][Ff][0-9][0-9]",fullname)
+                        match = re.search("[RrTtFfPp][FfPp][0-9][0-9]",fullname)
                         if not match:
                             print("Flight number not found in image path. Please")
                             print(" rename camera dirs to contain flight numbers")
@@ -679,7 +679,7 @@ if __name__ == "__main__":
         # List all the files/dirs in the working dir (sdir)
         dirfilelist = os.listdir(sdir)
         dirfilelist.sort()
-        pattern = re.compile(r'.*\/[FRT]F[0-1][0-9]$')
+        pattern = re.compile(r'.*\/[FRTP][FP][0-2][0-9]$')
         for file in dirfilelist:
             # Walk through the dirpath (this will ignore paths that
             # point to a file and

commit 0b70a97ba1b313b94305eaa8946d357048e28aa7

Fix bug introduced in commit 049fedb5. That commit allowed flights with a PP designation (in addition to RF,TF,FF) but missed one spot to make that update.

Configuration/scripts/archAC.py
@@ -289,7 +289,8 @@ class archRAFdata:
         p1 = subprocess.Popen(["/usr/bin/ncdump","-h",path], stdout=subprocess.PIPE)
         p2 = subprocess.Popen(["grep","FlightNumber"], stdin=p1.stdout, stdout=subprocess.PIPE)
         flightnum = (p2.communicate()[0].decode('utf-8').split('"')[1]).upper()
-        match= re.search('(\w(F|f)\d\d\w\w?\w?\d?\d?)', sfile)
+        match= re.search('(\w[FfPp]\d\d\w\w?\w?\d?\d?)', sfile)
+
         if match:
             flightnum2 = match[0].upper()
             if len(flightnum) >= 4: