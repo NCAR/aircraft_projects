39a720b76c7cb42f2354facf02f4ef6d3946f06f
Janine Aquino <janine@ucar.edu>
2026-08-13 13:35:40 -0600

    Only attempt movie generation for RF/TF/FF flights

--- scripts/data_flow/push_data.py
+++ scripts/data_flow/push_data.py
@@ -70,11 +70,14 @@ def main():
     setup.report(status, setup.PROJECT, setup.FLIGHT, setup.EMAIL, setup.FILE_EXT,final_message)
 
     # Now that the processing has completed, attempt to run the movie-generation script
-    try:
+    if setup.FLIGHT.startswith(('RF', 'TF', 'FF')):
+      try:
         script = "/home/local/aircraft_movies_animations/timeseries_animation.py"
         subprocess.run([sys.executable, script, "-f", setup.FLIGHT], check=True)
-    except:
+      except:
         print("Movie generation failed")
+    else:
+        print("Not an RF/TF/FF flight. Movie generation skipped."
 
 if __name__ == "__main__":
     main()
