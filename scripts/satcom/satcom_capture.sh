bf2eb06863e26275086fa6c1bdabf71d0051eb90
Janine Aquino <janine@ucar.edu>
2026-08-18 12:36:57 -0600

    Add date stamp to logfile name. Add script logging under /var/r1 and error handling

--- scripts/satcom/satcom_capture.sh
+++ scripts/satcom/satcom_capture.sh
@@ -1,17 +1,48 @@
 #!/bin/bash
-# Needs to run as root
-# Need to change interface to satcom interface onboard
+###############################################################################
+# Script to log traffic getting off the plane. Ignores traffic between
+# onboard hosts
+#  - Needs to run as root
+#  - Need to change interface to satcom interface onboard.
+# Options:
+#  - Excludes onboard traffic between .84 subnets from logging
+#  -i Interface
+#  -C, -W  Creates up to 10 100MB log files. Rotates logs
+#  -s Only snapshots 96 bytes of data, rather than the default 262144,
+#     to decrease logsize.
+###############################################################################
+# During INSPYRE:
 # - acserver: interface is eno8303np0
 # - MC brix01: enp3s0
 # - steam (for testing): enp0s31f6
-# Options:
-#  - Excludes onboard traffic between .84 subnets from logging
-#  -C, -W  Creates up to 10 1GB log files. Rotates logs
-#  -s Only snapshots 96 bytes of data, rather than the default 262144,
-#     to decrease logsize.
+######################################
+# CHANGE ME!!!
+interface="enp0s31f6"  # steam
+
+logfile="satcom_capture.log"
+output_dir="/var/r1/${PROJECT}/satcom-capture/"
+
+# Create logfile dir if needed
+if [ ! -d "$output_dir" ]; then
+    mkdir -p "$output_dir" ||
+      { echo "could not create $dir" >> "$output_dir/$logfile" 2>&1; exit 1; }
+fi
+
+# Stamp logfile with date/time script started
+date=$(date -u +%Y%m%d_%H%M%S)
+
+# Some interface sanity checking
+if [ ! -d "/sys/class/net/$interface" ]; then
+    echo "interface $interface not found" >> "$output_dir/$logfile" 2>&1
+    exit 1
+fi
+[ "$(cat /sys/class/net/$interface/operstate)" = "up" ] ||
+  { echo "$interface is down" >> "$output_dir/$logfile" 2>&1; exit 1; }
+
+
 tcpdump 'not (src net 192.168.84.0/24 and dst net 192.168.84.0/24)' \
-    -i enp0s31f6 \
-    -w /var/log/satcom-capture/traffic.pcap \
-    -C 1000 \
+    -i ${interface} \
+    -w /var/log/satcom-capture/traffic${date}.pcap \
+    -C 100 \
     -W 10 \
-    -s 96
+    -s 96 >> "$output_dir/$logfile" 2>&1
